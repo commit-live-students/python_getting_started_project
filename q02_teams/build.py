a318eab1a0b3192269793b864e97229e41a27912
gitint <surajgjadhav1988+gitint@gmail.com>
2017-09-29 09:44:58 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,14 +1,6 @@
-# default imports
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 
-from q01_read_data.build import load_data
-data = load_data()
-
-# solution
-def teams(data=data):
-
-    # write your code here
-    teams =
-
+def teams(data):
+    teams = data['info']['teams']
     return teams
