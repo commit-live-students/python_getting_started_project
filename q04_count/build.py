f548a406774aaea0eb0a43767d7b34048842e7b7
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:45:06 +0530

    Update build.py

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,15 +1,11 @@
 # Default Imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-
-from q01_read_data.build import read_data
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
-# Your Solution
-def deliveries_count(data):
-
-    # Write your code here
-
-
+# Your Solution Here
+def deliveries_count(data=data):
+    
+    # Your code here
+    
 
     return count
