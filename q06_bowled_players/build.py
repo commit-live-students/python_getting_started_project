865fe02cc109ec623a990c33eed193f580387b1e
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:54:20 +0530

    Update build.py

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -3,7 +3,7 @@ from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
-def bowled_out(data):
+def bowled_out(data=data):
 
     # Write your code here
 
