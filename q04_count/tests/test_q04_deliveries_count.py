24f55ab40d6716f282ecf17967da5119403d01f3
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:46:12 +0530

    Update test_q04_deliveries_count.py

--- q04_count/tests/test_q04_deliveries_count.py
+++ q04_count/tests/test_q04_deliveries_count.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from unittest import TestCase
-from q01_read_data.build import read_data
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
 from q04_count.build import deliveries_count
 
 class TestDeliveries_count(TestCase):
