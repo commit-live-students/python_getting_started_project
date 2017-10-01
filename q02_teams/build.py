f9891cc3a637291d1536997a79343294a61fa142
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:31:03 +0530

    Update build.py

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,8 +1,5 @@
 # default imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-
-from q01_read_data.build import read_data
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # solution
