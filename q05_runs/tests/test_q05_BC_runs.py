f9dcec7ee15e0faba8d685e930241eb9f7b15627
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:50:10 +0530

    Update test_q05_BC_runs.py

--- q05_runs/tests/test_q05_BC_runs.py
+++ q05_runs/tests/test_q05_BC_runs.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from unittest import TestCase
-from q01_read_data.build import read_data
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
 from q05_runs.build import BC_runs
 
 class TestBC_runs(TestCase):
