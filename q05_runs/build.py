0f2d7f1350036114e55ba55cd0a3a19c96dea147
Suraj Jadhav <surajhell88@gmail.com>
2017-10-02 00:49:24 +0530

    Update build.py

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,9 +1,7 @@
 # Default Imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-
-from q01_read_data.build import read_data
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
+
 
 # Your Solution
 def BC_runs(data):
