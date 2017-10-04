24c593b69812dd4e55677ced2a7d2d6d4ac5e15b
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-04 06:04:44 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,13 +1,16 @@
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
+import pprint
 data = read_data()
+name="hello"
 
 # Your Solution
 def first_batsman(data=data):
-
-    # Write your code here
-
-
-
-
+    deliveries = data['innings'][0]['1st innings']['deliveries'][0]
+    for key,val in deliveries.items():
+        if(key == 0.1):
+            global name
+            name= val['batsman']
+            break
     return name
+first_batsman(data)
