acc13c00ced5cf7383c007fbab8458c679546f9f
satishkhanna <snykhanna@gmail.com>
2017-10-05 03:52:03 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -2,10 +2,22 @@
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
+
+bowled_players = []
+
+#deliveries = data['innings'][1]['2nd innings']['deliveries']
+#print (deliveries)
+
 # Your Solution
 def bowled_out(data=data):
-
-    # Write your code here
-
-
-    return bowled_players
+    deliveries = data['innings'][1]['2nd innings']['deliveries']
+    #print (deliveries)
+    for delivery in deliveries:
+        for deliver_key, deliver_val in delivery.items():
+            if 'wicket' in deliver_val:
+                if deliver_val['wicket']['kind']=='bowled':
+                    #count = 1
+                    #wicket.append(count)
+                    bowledbatsman = deliver_val['wicket']['player_out']
+                    bowled_players.append(bowledbatsman)
+    return (bowled_players)
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,24 @@
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
+first_innings = []
+second_innings = []
 
 # Your Solution
 def extras_runs(data=data):
+    first_deliveries = data['innings'][0]['1st innings']['deliveries']
+    for delivery in first_deliveries:
+        for deliver_key, deliver_val in delivery.items():
+                first_inning_extra = deliver_val['runs']['extras']
+                first_innings.append (first_inning_extra)
+    extra_first = sum(first_innings)
+    second_deliveries = data['innings'][1]['2nd innings']['deliveries']
+    for delivery in second_deliveries:
+        for deliver_key, deliver_val in delivery.items():
+                second_inning_extra = deliver_val['runs']['extras']
+                second_innings.append (second_inning_extra)
+    extra_second = sum(second_innings)
 
-    # Write your code here
-
-
-    difference =
-
+    difference = extra_second - extra_first
 
     return difference
