9537fb2a20df1a0eaec3661fa2db61c944f95126
satishkhanna <snykhanna@gmail.com>
2017-10-05 07:43:03 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,19 +4,24 @@ data = read_data()
 first_innings = []
 second_innings = []
 
+
+
 # Your Solution
 def extras_runs(data=data):
     first_deliveries = data['innings'][0]['1st innings']['deliveries']
     for delivery in first_deliveries:
         for deliver_key, deliver_val in delivery.items():
-                first_inning_extra = deliver_val['runs']['extras']
-                first_innings.append (first_inning_extra)
+            if 'extras' in deliver_val:
+                counter = 1
+                first_innings.append(counter)
     extra_first = sum(first_innings)
+
     second_deliveries = data['innings'][1]['2nd innings']['deliveries']
     for delivery in second_deliveries:
         for deliver_key, deliver_val in delivery.items():
-                second_inning_extra = deliver_val['runs']['extras']
-                second_innings.append (second_inning_extra)
+            if 'extras' in deliver_val:
+                counter = 1
+                second_innings.append(counter)
     extra_second = sum(second_innings)
 
     difference = extra_second - extra_first
