3c2dcef4d5dacaf4c815cd25e3e84662b2370d47
swapnilrt <swapnil13488@gmail.com>
2017-10-05 04:51:31 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -2,7 +2,7 @@
 import yaml
 
 def read_data():
-    with open('./data/ipl_match.yaml') as f:
+    with open('../data/ipl_match.yaml') as f:
         data = yaml.load(f)
     # return data variable
     return data
