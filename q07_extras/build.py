5cd6f639e8e768a6331cd79298aad97ebfbb96ad
swapnilrt <swapnil13488@gmail.com>
2017-10-08 06:41:28 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -5,10 +5,22 @@ data = read_data()
 # Your Solution
 def extras_runs(data=data):
 
-    # Write your code here
+    extras_first_innings=0
+    extras_second_innings=0
+    deliveries_first = data['innings'][0]['1st innings']['deliveries']
+    deliveries_second = data['innings'][1]['2nd innings']['deliveries']
+
+    for delivery in deliveries_first:
+        for delivery_number, delivery_info in delivery.items():
+                     if delivery_info['runs']['extras'] !=0:
+                        extras_first_innings+=1
 
 
-    difference =
+    for delivery in deliveries_second:
+        for delivery_number, delivery_info in delivery.items():
+                     if delivery_info['runs']['extras'] !=0:
+                        extras_second_innings+=1
+    difference =extras_second_innings-extras_first_innings
 
 
     return difference
