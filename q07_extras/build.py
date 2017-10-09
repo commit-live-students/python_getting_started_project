6f10f745a4c17380063f10edb20c0720f26ea591
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-09 07:30:47 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -8,25 +8,15 @@ def extras_runs(data=data):
     inning1_extras=0
     for i in d1:
         for delivery_no ,delivery_info in i.items():
-            inning1_extras = inning1_extras + delivery_info['runs']['extras']
-
-
-
+            if (delivery_info['runs']['extras'] >= 1):
+                inning1_extras = inning1_extras + 1
 
     d2 = data['innings'][1]['2nd innings']['deliveries']
     inning2_extras=0
     for i in d2:
-        for delivery_no ,delivery_info in i.items():
-            inning2_extras = inning2_extras + delivery_info['runs']['extras']
-
-
+           for delivery_no ,delivery_info in i.items():
+                 if (delivery_info['runs']['extras'] >= 1):
+                    inning2_extras = inning2_extras + 1
 
     difference = inning2_extras - inning1_extras
-
-
-
-
-    
-
-
     return difference
