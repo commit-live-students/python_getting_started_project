bdc7b6e2331faf2d0737fe0f8f90df21cb5d3b89
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-09 07:14:30 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -4,8 +4,24 @@ data = read_data()
 
 # Your Solution
 def bowled_out(data=data):
+    deliveries = data['innings'][1]['2nd innings']['deliveries']
+    a=[]
+    for i in deliveries:
+        for delivery , delivery_info in i.items() :
+            a.append(delivery_info.get('wicket'))
 
-    # Write your code here
+    b = []
+    for i in a:
+        if i != None:
+            b.append(i)
+
+    bowled_players = []
+
+    for i in b:
+        if i['kind'] == 'bowled':
+            bowled_players.append(i['player_out'])
+
+    
 
 
     return bowled_players
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,29 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    d1 = data['innings'][0]['1st innings']['deliveries']
+    inning1_extras=0
+    for i in d1:
+        for delivery_no ,delivery_info in i.items():
+            inning1_extras = inning1_extras + delivery_info['runs']['extras']
 
-    # Write your code here
 
 
-    difference =
+
+    d2 = data['innings'][1]['2nd innings']['deliveries']
+    inning2_extras=0
+    for i in d2:
+        for delivery_no ,delivery_info in i.items():
+            inning2_extras = inning2_extras + delivery_info['runs']['extras']
+
+
+
+    difference = inning2_extras - inning1_extras
+
+
+
+
+    
 
 
     return difference
