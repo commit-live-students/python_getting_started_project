9b87d1b5a32bc62364594e6bf2a7e8f33725224e
Rahul0211 <rahuljain86@hotmail.com>
2017-10-14 10:53:00 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -4,10 +4,24 @@ data = read_data()
 
 # Your Solution
 def first_batsman(data=data):
+    innings=data['innings']
+    for i in innings:
+        inning=i
+        for n in inning:
+            delivaries = inning[n]
+            for d in delivaries:
+                balls= delivaries[d]
+                for b in balls:
+                    first_ball= b
+                    for bat in first_ball:
+                        first_bats = first_ball[bat]
+                        a = first_bats
+                        break
+                    break
+                break
+            break
+        break
 
-    # Write your code here
-
-
-
-
-    return name
+    return a['batsman']
+name=first_batsman()
+print name
