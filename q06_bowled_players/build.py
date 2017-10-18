ec7ae1cccb1bf4910b5fab713f02e76238648864
vinod-boga <vinodboga17@gmail.com>
2017-10-18 18:14:20 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -4,8 +4,15 @@ data = read_data()
 
 # Your Solution
 def bowled_out(data=data):
-
-    # Write your code here
-
+    import numpy as np
+    import pandas as pd
+    data1 = data['innings'][1]
+    data2 = data1['2nd innings']
+    data3 = data2['deliveries']
+    data4 = pd.concat([pd.DataFrame(l) for l in data3],axis=1).T
+    wicket1 = data4[data4['wicket'].notnull()]['wicket']
+    wicket_STd = pd.Series.to_dict(wicket1)
+    wicket_dTD = pd.DataFrame.from_dict(wicket_STd,orient = 'index')
+    bowled_players =  list(wicket_dTD[wicket_dTD['kind'] == 'bowled']['player_out'])
 
     return bowled_players
