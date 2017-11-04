9c4fcde8dfcdc0e0dbe9c82296408c5e900c4b0f
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-11-04 15:45:08 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -6,7 +6,14 @@ data = read_data()
 # Your Solution
 def BC_runs(data):
 
-    # Write your code here
+    runs = 0
+    first_inning = data['innings'][0]['1st innings']['deliveries']
+    for deliveries in first_inning:
+        for delivery in deliveries:
+            batsman_name = deliveries[delivery]['batsman']
+            if batsman_name == 'BB McCullum':
+                runs = runs + deliveries[delivery]['runs']['batsman']
 
-
+            break
     return(runs)
+    
