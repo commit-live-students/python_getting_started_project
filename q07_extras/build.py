b4dc6051e1478512ccc3df22cec22ed3a85bdab4
ankutty <akutty@deloitte.com>
2017-11-06 18:06:24 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,9 +6,14 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    first_innings = data['innings'][0]['1st innings']['deliveries']
+    second_innings = data['innings'][1]['2nd innings']['deliveries']
 
+    first_innigs_extras = [d.values()[0]['runs']['extras'] for d in first_innings]
+    second_innings_extras = [d.values()[0]['runs']['extras'] for d in second_innings]
 
-    difference =
+    extra1= len([extras for extras in first_innigs_extras if extras > 0])
+    extra2= len([extras for extras in second_innings_extras if extras > 0])
 
-
+    difference = extra2 - extra1
     return difference
