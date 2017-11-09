72e1844ec5d3a4a17b569611c796a4fa73e24fe3
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-11-09 17:45:12 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,15 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-
-    # Write your code here
-
-
-    difference =
-
+    extras_1, extras_2 = 0, 0
+    for delivery in data['innings'][0]['1st innings']['deliveries']:
+        delivery_content = delivery[next(iter(delivery))]
+        if 'extras' in delivery_content:
+            extras_1 += 1
+    for delivery in data['innings'][1]['2nd innings']['deliveries']:
+        delivery_content = delivery[next(iter(delivery))]
+        if 'extras' in delivery_content:
+            extras_2 += 1
+    difference = extras_2 - extras_1
 
     return difference
