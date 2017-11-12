260861592fe97b6d2e3a9e84db60cfed2dd2bc66
gupta1006 <guptagauttam@gmail.com>
2017-11-12 07:35:46 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -2,6 +2,6 @@ from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 def teams(data=data):
-
-    teams = data['info']['teams']
-    return teams
+    team_names = data['info']['teams']
+    return team_names
+    print(team_names)
