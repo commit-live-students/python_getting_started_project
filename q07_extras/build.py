f7a3c047f7cfa72ceb2f8e1a3dbae72113b95a9b
chirag-shinde <shinde.chirag2204@gmail.com>
2017-11-12 04:03:15 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,17 +6,15 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
-        # Write your code here
     difference = 0
     for i in range(len(data['innings'])):
         for key in data['innings'][i]:
             for delivery_number in data['innings'][i][key]['deliveries']:
                 if 'extras' in delivery_number.values()[0]:
-                    for extra in delivery_number.values()[0]['extras']:
-                        if i % 2 == 0:
-                            difference -= len(delivery_number.values()[0]['extras'].keys())
-                        else:
-                            difference += len(delivery_number.values()[0]['extras'].keys())
+                    if i % 2 == 0:
+                        difference -= len(delivery_number.values()[0]['extras'].keys())
+                    else:
+                        difference += len(delivery_number.values()[0]['extras'].keys())
 
 
     return abs(difference)
