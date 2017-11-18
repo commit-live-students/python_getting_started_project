11a45a6ac977307d295418f8e7ce113dfe15423a
harshavachhani <harshavachhani@gmail.com>
2017-11-18 18:40:13 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -7,6 +7,13 @@ data = read_data()
 def BC_runs(data):
 
     # Write your code here
+    deliveries=data['innings'][0]['1st innings']['deliveries']
+    bm=[]
+    for index in range(len(deliveries)):
+        delivery=deliveries[index]
+        for delivery_number, delivery_info in delivery.items():
+            if delivery_info['batsman'] == 'BB McCullum':
+                runs_scored = delivery_info['runs']['batsman']
+                bm.append( runs_scored)
 
-
-    return(runs)
+    return sum(bm)
