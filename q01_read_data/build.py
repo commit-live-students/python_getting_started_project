e898112334cbf10982f508cd497f9f624f3ffcf5
minalpotekar15 <minupotekar15@gmail.com>
2017-11-18 20:10:32 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -6,7 +6,12 @@ def read_data():
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
     # Write your code here
 
-    data =
+    #data = '../data/ipl_match.yaml'
+    #data = csv.DictReader(open('ipl_match.yaml', 'r'))
+    with open('./data/ipl_match.yaml', 'r') as f:
+        data = yaml.load(f)
+
+    #data = yaml.load('./data/ipl_match.yaml')
 
     # return data variable
     return data
