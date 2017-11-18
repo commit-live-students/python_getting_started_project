0aabc70f1fbdcb76c57a8bcb2338c60952151882
vp999 <patilvish@gmail.com>
2017-11-18 09:01:53 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -4,8 +4,11 @@ data = read_data()
 
 # Your Solution
 def bowled_out(data=data):
-
     # Write your code here
-
-
+    bowled_players = []
+    for deliveries in data['innings'][1]['2nd innings']['deliveries']:
+        for delivery in deliveries:
+            if 'wicket' in deliveries[delivery]:
+                if deliveries[delivery]['wicket']['kind'] == 'bowled':
+                    bowled_players.append( deliveries[delivery]['wicket']['player_out'])
     return bowled_players
