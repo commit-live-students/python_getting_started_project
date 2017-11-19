a032b4aca3ce790ed0a092dded7127c00d635b24
KRSatpute <ksatpute123@gmail.com>
2017-11-19 19:10:53 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,20 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    innings = [inning for _, inning in enumerate(data["innings"])]
 
-    # Write your code here
+    inning_overs = [inning[0] for inning in
+                    [innings[x].values()[0].values() for x in range(2)]]
+                    # range(2) coz only 2 inninngs per games
 
+    innings_extras = []
+    for deliveries in inning_overs:
+        innings_extras.append(len(
+            [v.values()[0]["runs"]["extras"] for i, v in enumerate(deliveries)
+             if v.values()[0]["runs"]["extras"] > 0]
+        ))
 
-    difference =
-
+    difference = [abs(innings_extras[i + 1] - innings_extras[i])
+                  for i in range(len(innings_extras) - 1)][0]
 
     return difference
