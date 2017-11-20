59f10f370f13dd70bce731687579e80459b43981
swapnilpatil22 <swapnil.patil2205@hotmail.com>
2017-11-20 15:14:18 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -6,7 +6,13 @@ data = read_data()
 # Your Solution
 def BC_runs(data):
 
-    # Write your code here
+    deliveries_list=data["innings"][0]["1st innings"]["deliveries"]
+    run=0
+    for target_list in deliveries_list:
+     for overs in target_list:
+        if "BB McCullum"==target_list[overs]["batsman"]:
 
+            if target_list[overs]["runs"]["batsman"]!=0:
+                run=run+target_list[overs]["runs"]["batsman"]
 
-    return(runs)
+    return run
