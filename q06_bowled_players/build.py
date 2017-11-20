10a1b9e6c1892a2afd28a2a5b84185ccebb0540f
shashank-kanojia <shasha_nk@yahoo.co.in>
2017-11-20 19:17:51 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -7,5 +7,15 @@ def bowled_out(data=data):
 
     # Write your code here
 
+    bowled_players = []
+
+    for innings in range(len([data][0]['innings'])):
+        for inn_name, inn_details in [data][0]['innings'][innings].items():
+            for attr_name, del_info in inn_details.items():
+                if attr_name <> 'team':
+                    for del_details in del_info:
+                        for delivery, delivery_outcome in del_details.items():
+                            if 'wicket' in delivery_outcome and delivery_outcome['wicket']['kind'] == 'bowled':
+                                bowled_players.append(delivery_outcome['wicket']['player_out'])
 
     return bowled_players
