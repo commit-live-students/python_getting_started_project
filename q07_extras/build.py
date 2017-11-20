428332f6562fd8ddf5374c8609158a91be481a17
shashank-kanojia <shasha_nk@yahoo.co.in>
2017-11-20 19:31:51 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -7,8 +7,18 @@ def extras_runs(data=data):
 
     # Write your code here
 
+    extras_per_innings = [0,0]
 
-    difference =
+    for innings in range(len([data][0]['innings'])):
+        for inn_name, inn_details in [data][0]['innings'][innings].items():
+            for attr_name, del_info in inn_details.items():
+                if attr_name <> 'team':
+                    for del_details in del_info:
+                        for delivery, delivery_outcome in del_details.items():
+                            if 'extras' in delivery_outcome:
+                                extras_per_innings[innings] += 1
+
+    difference = extras_per_innings[1] - extras_per_innings[0]
 
 
     return difference
