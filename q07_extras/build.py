f0a333715784cf8d1e03edaccc19af33702790a8
dharmil25 <dharmil2524_shah@ymail.com>
2017-11-20 07:08:33 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,9 +6,35 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+# Code your solution here
+
+    extras_runs1 = []
+    extras_runs2 = []
+    deliveries1 = data['innings'][0]['1st innings']['deliveries']
+    deliveries2 = data['innings'][1]['2nd innings']['deliveries']
+
+    for delivery in deliveries1:
+        for delivery_number, delivery_info in delivery.items():
+            if delivery_info['runs']['extras'] > 0:
+                over_and_ball_number = delivery_number
+                runs_scored = delivery_info['runs']['extras']
+
+                extras_runs1.append((runs_scored))
 
 
-    difference =
+
+    for delivery in deliveries2:
+        for delivery_number, delivery_info in delivery.items():
+            if delivery_info['runs']['extras'] > 0:
+                over_and_ball_number = delivery_number
+                runs_scored = delivery_info['runs']['extras']
+
+                extras_runs2.append((runs_scored))
+
+    s1 = len(extras_runs1)
+    s2 = len(extras_runs2)
+
+    difference = s2-s1
 
 
     return difference
