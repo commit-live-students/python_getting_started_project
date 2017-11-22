015d3e7159f44f78244b60ffe535cfafe48fae95
palash1512 <palash9515@gmail.com>
2017-11-22 03:48:51 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,9 +6,37 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    count_1st=0
+    count_2nd=0
+    item = data['innings'][0]['1st innings']['deliveries']
+    for i in range(0,len(item)):
+        key1 = item[i].keys()[0]
+        key2 = item[i][key1].keys()[3]
+        if( key2 == 'extras'):
+            key3 = item[i][key1][key2].keys()[0]
+            statment = item[i][key1][key2][key3]
 
+            if( key3 == 'legbyes' ):
+                extra_run = int( statment )
+                count_1st = count_1st + extra_run
+            elif( key3 == 'wides' ):
+                extra_run = int( statment )
+                count_1st = count_1st + extra_run
 
-    difference =
+    item = data['innings'][1]['2nd innings']['deliveries']
+    for i in range(0,len(item)):
+        key1 = item[i].keys()[0]
+        key2 = item[i][key1].keys()[3]
+        if( key2 == 'extras'):
+            key3 = item[i][key1][key2].keys()[0]
+            statment = item[i][key1][key2][key3]
 
+            if( key3 == 'legbyes' ):
+                extra_run = int( statment )
+                count_2nd = count_2nd + extra_run
+            elif( key3 == 'wides' ):
+                extra_run = int( statment )
+                count_2nd = count_2nd + extra_run
 
+    difference =count_2nd-count_1st
     return difference
