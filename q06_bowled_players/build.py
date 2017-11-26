9ff3769cae92d7bc6933566bad4797670b00cdc3
dharmil25 <dharmil2524_shah@ymail.com>
2017-11-26 06:53:59 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -7,5 +7,15 @@ def bowled_out(data=data):
 
     # Write your code here
 
+    bowled_players = []
+
+    deliveries = data['innings'][1]['2nd innings']['deliveries']
+    for delivery in deliveries:
+        for delivery_number, delivery_info in delivery.items():
+            if 'wicket' in delivery_info:
+                if delivery_info['wicket']['kind'] == 'bowled':
+                    bowled_out = delivery_info['wicket']['player_out']
+
+                    bowled_players.append(bowled_out)
 
     return bowled_players
