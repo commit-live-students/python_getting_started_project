7dee04428dd03c65cb37f978df1fad637db376f9
tikiabbas <abbas.tiki@gmail.com>
2017-11-30 19:17:41 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,17 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-
-    # Write your code here
-
-
-    difference =
-
-
-    return difference
+    innf = data["innings"][0]["1st innings"]["deliveries"]
+    inns = data["innings"][1]["2nd innings"]["deliveries"]
+    extraf = 0
+    extrasec = 0
+    for i in range (0,len(innf)):
+        delv = innf[i]
+        if delv[delv.keys()[0]].get('extras',None) != None:
+            extraf = extraf + 1
+    for i in range (0,len(inns)):
+        delv = inns[i]
+        if delv[delv.keys()[0]].get('extras',None) != None:
+            extrasec = extrasec + 1
+    difference = extrasec - extraf
+    return difference   
