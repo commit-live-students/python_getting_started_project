e7b56939674144e30746a9ba9f5b04e18c13f7cf
amol120586 <amol.24365@gmail.com>
2017-11-30 09:23:11 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -6,8 +6,12 @@ data = read_data()
 def first_batsman(data=data):
 
     # Write your code here
+    data = data['innings']
 
+    data = [i['1st innings'] for i in data if '1st innings' in i]
 
+    data = [i['deliveries'] for i in data if 'deliveries' in i]
 
+    name = data[0][0][0.1]['batsman']
 
     return name
