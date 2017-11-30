4d278b28517a0f4e4a8d7a4fdcd5d5e66a7ff631
amol120586 <amol.24365@gmail.com>
2017-11-30 11:13:55 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -7,6 +7,17 @@ data = read_data()
 def BC_runs(data):
 
     # Write your code here
+    runs = 0
 
+    data = data['innings']
+
+    data = [i['1st innings'] for i in data if '1st innings' in i]
+
+    data = [i['deliveries'] for i in data if 'deliveries' in i]
+
+    for l in data[0]:
+        for i in l:
+            if l[i]['batsman'] == 'BB McCullum':
+                 runs += l[i]['runs']['batsman']
 
     return(runs)
