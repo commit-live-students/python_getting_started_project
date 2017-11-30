6020a8618cf673a6c4b1de63cabd4fd38d09c6aa
amol120586 <amol.24365@gmail.com>
2017-11-30 10:49:02 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -4,8 +4,19 @@ data = read_data()
 
 # Your Solution Here
 def deliveries_count(data=data):
-    
+
     # Your code here
-    
+    count = 0
+
+    data = data['innings']
+
+    data = [i['1st innings'] for i in data if '1st innings' in i]
+
+    data = [i['deliveries'] for i in data if 'deliveries' in i]
+
+    for l in data[0]:
+        for i in l:
+            if l[i]['batsman'] == 'RT Ponting':
+                count += 1
 
     return count
