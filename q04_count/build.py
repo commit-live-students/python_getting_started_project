a4abdf1520c7fb00e6ceaacb37cda38c0b22d366
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2017-12-04 02:28:18 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -4,8 +4,18 @@ data = read_data()
 
 # Your Solution Here
 def deliveries_count(data=data):
-    
+
     # Your code here
-    
+    count = 0
+    dely = data['innings'][0]['1st innings']['deliveries']
+    loop_var = 0
+    for batsman in dely:
+        ind = data['innings'][0]['1st innings']['deliveries'][loop_var].keys()
+        batsman = data['innings'][0]['1st innings']['deliveries'][loop_var][ind[0]]['batsman']
+        if batsman == 'RT Ponting':
+            count=count + 1
+        loop_var=loop_var + 1
 
     return count
+
+print deliveries_count()
