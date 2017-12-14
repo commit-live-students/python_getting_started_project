9a1c767d023986d8e4c174e62106da92f132fcd3
Dhivya-sekar <dhivya.SK@gmail.com>
2017-12-14 07:26:47 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,11 +1,6 @@
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
-# solution
 def teams(data=data):
-
-    # write your code here
-    #teams =
-
-    return teams
+    team = data['info']['teams']
+    return team
