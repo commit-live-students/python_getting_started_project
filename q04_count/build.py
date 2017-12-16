f0793491ea9e471142a2e44ca921e76904e1273c
Abiraame <abiraamet@gmail.com>
2017-12-16 14:52:23 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,11 +1,18 @@
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
-# Your Solution Here
 def deliveries_count(data=data):
-    
-    # Your code here
-    
-
+    d = data['innings'][0]['1st innings']['deliveries']
+    n = 0
+    count = 0
+    while n <= 123:
+        ls = d[n]
+        for k in ls:
+            nw = ls[k]
+            for s in nw:
+                nm = nw['batsman']
+        if nm == 'RT Ponting':
+            count = count + 1
+    n = n + 1
     return count
+print count
