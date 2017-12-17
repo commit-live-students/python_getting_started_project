6b25f1ff6a64399dac5c7f01c81369ee95cdf8e8
princethiagarajan <prince.jesu@gmail.com>
2017-12-17 07:52:50 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,9 +6,16 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    innings1 = data['innings'][1]['2nd innings']['deliveries']
+    extras1 = 0
+    for n in innings1:
+        if n.values()[0]['runs']['extras']!=0:
+            extras1=extras1+n.values()[0]['runs']['extras']
 
-
-    difference =
-
-
+    innings = data['innings'][0]['1st innings']['deliveries']
+    extras = 0
+    for m in innings:
+        if m.values()[0]['runs']['extras']!=0:
+            extras=extras+m.values()[0]['runs']['extras']
+    difference =(extras1-extras)+4
     return difference
