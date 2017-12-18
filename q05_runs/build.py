14e61e657a18c4900570e5d5dc519f24f467f899
Vijay1306 <vijaymadurai@yahoo.com>
2017-12-18 18:46:31 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,12 @@
-# Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 
 # Your Solution
 def BC_runs(data):
-
-    # Write your code here
-
-
+    subset_data = data['innings'][0]['1st innings']['deliveries']
+    runs = 0
+    for i in subset_data:
+        if i.values()[0]['batsman'] == 'BB McCullum':
+            runs = runs + i.values()[0]['runs'].values() [0]
     return(runs)
