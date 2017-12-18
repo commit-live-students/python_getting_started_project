49a1ff05d99000f8dc26394eab058ee405eb4e39
sjhonny03 <SJHONNY03@gmail.com>
2017-12-18 04:25:00 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -5,10 +5,23 @@ data = read_data()
 # Your Solution
 def extras_runs(data=data):
 
-    # Write your code here
+    extras1=[]
+    extras2=[]
+    deliveries1=data['innings'][0]['1st innings']['deliveries']
+    deliveries2=data['innings'][1]['2nd innings']['deliveries']
+    for balls1 in deliveries1:
+        if 'extras' in  balls1.values()[0].keys():
+            extras= balls1.values()[0]['extras'].values()[0]
+            extras1.append(extras)
 
 
-    difference =
+    for balls2 in deliveries2:
+        if 'extras' in  balls2.values()[0].keys():
+            extras= balls2.values()[0]['extras'].values()[0]
+            extras2.append(extras)
+
+        difference=len(extras2)-len(extras1)
+
 
 
     return difference
