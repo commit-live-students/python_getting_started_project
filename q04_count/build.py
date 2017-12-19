5162fc29c1803aa7d66e32fcfb905aab02a87084
Vanchipalanisamy <VANCHINATHAN.PALANISAMY@hotmail.com>
2017-12-19 15:27:01 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -4,8 +4,11 @@ data = read_data()
 
 # Your Solution Here
 def deliveries_count(data=data):
-    
-    # Your code here
-    
 
-    return count
+    # Your code here
+    data1 = data['innings'][0]['1st innings']['deliveries']
+    nos_of_delivery = 0
+    for i in data1:
+        if i.values()[0]['batsman']=='RT Ponting':
+            nos_of_delivery = nos_of_delivery+1
+    return nos_of_delivery
--- q05_runs/build.py
+++ q05_runs/build.py
@@ -5,8 +5,10 @@ data = read_data()
 
 # Your Solution
 def BC_runs(data):
-
     # Write your code here
-
-
-    return(runs)
+            data1 = data['innings'][0]['1st innings']['deliveries']
+            runs = 0
+            for i in data1:
+                        if i.values()[0]['batsman'] == 'BB McCullum':
+                                    runs = runs + i.values()[0]['runs'].values()[0]
+            return(runs)
