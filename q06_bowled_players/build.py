2f9bfc13084844129d535d4b9030c34ae9b92833
Abiraame <abiraamet@gmail.com>
2017-12-19 17:29:26 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,11 +1,14 @@
-# Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
-# Your Solution
+deliveries =  data['innings'][1]['2nd innings']['deliveries']
+bowled_players = list()
 def bowled_out(data=data):
-
-    # Write your code here
-
-
+    for balls in deliveries:
+        inter = balls.values()[0]
+        for i in inter:
+            if i == 'wicket':
+                if inter['wicket']['kind'] == 'bowled':
+                    trial = inter['wicket']['player_out']
+                    bowled_players[:0]=[trial]
+    bowled_players.sort()
     return bowled_players
