e2a60f53ee63f568a293cb210d05951ed329f198
RohinSequeira <sequeira.rohin@gmail.com>
2017-12-23 15:01:42 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,9 +6,23 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    req_data1 = data['innings'][0]['1st innings']['deliveries']
+    req_data2 = data['innings'][1]['2nd innings']['deliveries']
+    extras1 = []
+    extras2 = []
+    for i in req_data1:
+        overs = i.items()
+        for ball in overs:
+            if (ball[1]['runs'].get('extras') != 0):
+                extras1.append(ball[1]['runs'].get('extras'))
 
+    for i in req_data2:
+        overs = i.items()
+        for ball in overs:
+            if(ball[1]['runs'].get('extras') != 0):
+                extras2.append(ball[1]['runs'].get('extras'))
 
-    difference =
+    difference = abs(len(extras1) - len(extras2))
 
 
     return difference
