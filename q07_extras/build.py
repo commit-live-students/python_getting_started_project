3f39cb74a6231a1f475ff442526cb1c0fe3743c5
viishnu712 <viishnu712@gmail.com>
2018-01-20 04:10:28 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,38 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    extras_first_innings=[]
+    extras_second_innings=[]
+    new1=[]
+    new2=[]
+    for balls in data['innings'][0]['1st innings']['deliveries']:
+        for key,values in balls.items():
+            try:
+                new_value = values['extras']
+                extras_first_innings.append(list(new_value.values()))
+                #print (extras_first_innings)
+            except KeyError:
+        # Key is not present
+                pass
+    for i in extras_first_innings:
+        new1.append (i[0])
 
-    # Write your code here
+    for balls in data['innings'][1]['2nd innings']['deliveries']:
+        for key,values in balls.items():
+            try:
+                new_value = values['extras']
+                list(new_value.values())
+                extras_second_innings.append(list(new_value.values()))
+            except KeyError:
+        # Key is not present
+                pass
+
+    for i in extras_second_innings:
+        new2.append (i[0])
 
 
-    difference =
+
+    difference =abs(len(new1)-len(new2))
 
 
     return difference
