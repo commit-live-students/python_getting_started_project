73440937d6d17e2523f5733707ff7059fbce1481
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-01-21 04:13:53 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -8,10 +8,9 @@ def bowled_out(data=data):
     # Write your code here
     bowled_players = []
     for i in range(0,len(data['innings'][1]['2nd innings']['deliveries'])):
-        keys1 = data['innings'][1]['2nd innings']['deliveries'][i].keys()
-        for j in range(0,len(keys1)):
-            if 'wicket' in data['innings'][1]['2nd innings']['deliveries'][i][keys1[j]].keys():
-                if data['innings'][1]['2nd innings']['deliveries'][i][keys1[j]]['wicket']['kind'] == 'bowled':
-                    bowled_players.append(data['innings'][1]['2nd innings']['deliveries'][i][keys1[j]]['batsman'])
+        keys1 = data['innings'][1]['2nd innings']['deliveries'][i].keys()[0]
+        if 'wicket' in data['innings'][1]['2nd innings']['deliveries'][i][keys1].keys():
+            if data['innings'][1]['2nd innings']['deliveries'][i][keys1]['wicket']['kind'] == 'bowled':
+                bowled_players.append(data['innings'][1]['2nd innings']['deliveries'][i][keys1]['batsman'])
 
     return bowled_players
