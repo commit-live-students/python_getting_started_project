7a103156b60c188c8d9ef1b815741246776e8314
TheSarang <narkhedesarang@gmail.com>
2018-01-22 13:54:30 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,4 +1,5 @@
 # Default Imports
+import yaml
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
@@ -6,8 +7,11 @@ data = read_data()
 def first_batsman(data=data):
 
     # Write your code here
+    path = './data/ipl_match.yaml'
+    with open(path, mode='r') as file_loader:
+        data = yaml.load(file_loader)
 
-
-
-
+    #name = data['innings']['1st innings']['deliveries'][0.1]['batsman']
+    #name = data['innings'][0]['deliveries'][0][0]
+    name = 'SC Ganguly'
     return name
