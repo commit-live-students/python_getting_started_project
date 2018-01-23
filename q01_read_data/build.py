e350f4d136f7c08be2845fc43d9c2e321a531982
madhav2712 <mishra.madhav4@gmail.com>
2018-01-23 07:23:41 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,12 +1,17 @@
 import yaml
 
-def read_data():
+#def read_data(path='C:\Users\Madhav.Mishra\.atom\.commit-live\code\python_getting_started_project\data\ipl_match.yaml'):
+def read_data(path='./data/ipl_match.yaml'):
+    with open(path, 'r') as f:
+        data=yaml.load(f)
+    return data
+
 
     # import the csv file into `data` variable
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
     # Write your code here
 
-    data =
+    #data =yaml.load(f)
 
     # return data variable
     return data
