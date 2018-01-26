c80b59d6deacdb8abdb1be57e15d1385b0668be4
kvin15 <vinayakkhamkar@gmail.com>
2018-01-26 15:38:23 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -4,8 +4,31 @@ data = read_data()
 
 # Your Solution Here
 def deliveries_count(data=data):
-    
-    # Your code here
-    
+    s_batsman = "RT Ponting"
+    # stores the deliveries faced by the batsman
+    i_count = 0
+    i_runs_scored = 0
+    innings_list = data["innings"]
+    i = 0
+    while True:
+        if i >= 2:
+            break
+        elif i == 0:
+            inning_details_lst = innings_list[0]["1st innings"]["deliveries"]
+            i = i + 1
+        else:
+            inning_details_lst = innings_list[1]["2nd innings"]["deliveries"]
+            i = i + 1
 
-    return count
+        # parse the inning_details
+        for delivery_dtl in inning_details_lst:
+            for delivery_value in delivery_dtl.itervalues():
+                if delivery_value["batsman"] ==  s_batsman:
+                    i_runs_scored = i_runs_scored + delivery_value["runs"]["batsman"]
+                    i_count = i_count + 1
+                    if  "wicket" in delivery_value:
+                        # if given batsman is out on delivery then break
+                        # assuming the deliveries are in sequence
+                        break
+
+    return i_count
