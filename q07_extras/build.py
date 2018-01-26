9683912e6be9c0b4939c64faf1dc0038cae43943
siddhichandan <siddhichandan@gmail.com>
2018-01-26 15:59:00 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -7,8 +7,10 @@ def extras_runs(data=data):
 
     # Write your code here
 
+    deliveries1 = data['innings'][0]['1st innings']['deliveries']
+    deliveries2 = data['innings'][1]['2nd innings']['deliveries']
 
-    difference =
-
-
+    difference = abs(
+        len([ v['runs']['extras'] for d in deliveries1 for k,v in d.items() if v['runs']['extras']>0 ])
+            -len([ v['runs']['extras'] for d in deliveries2 for k,v in d.items() if v['runs']['extras']>0]))
     return difference
