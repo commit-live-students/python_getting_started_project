18a963b0e91f13a20e251cec1e07e601a6c6b628
kvin15 <vinayakkhamkar@gmail.com>
2018-01-26 16:08:19 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -6,6 +6,19 @@ data = read_data()
 def bowled_out(data=data):
 
     # Write your code here
+    bowled_players = []
+    innings_list = data["innings"]
 
+    inning_details_lst = innings_list[1]["2nd innings"]["deliveries"]
+
+    # parse the inning_details
+    # find the batsman bowled out in second innings
+    for delivery_dtl in inning_details_lst:
+        for delivery_value in delivery_dtl.itervalues():
+            if  "wicket" in delivery_value:
+                if delivery_value["wicket"]["kind"] == "bowled":
+                    # if given batsman is out on delivery then break
+                    # assuming the deliveries are in sequence
+                    bowled_players.append(delivery_value["batsman"])
 
     return bowled_players
