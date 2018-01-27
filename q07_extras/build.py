b2fcf890af95747b572f268160f6ca05ae6152ba
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-01-27 09:59:24 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -6,9 +6,32 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    first_innings = [] #list for storing extra runs of 1st innings
+    second_innings = []#list for storing extra runs of 2nd innings
+    sum1 = 0 #addition of all extra runs in 1st innings
+    sum2 = 0 #addition of all extra runs in 2nd innings
+    difference = 0 #difference of extra runs between 1st and 2nd innings
+    deliv1 = data['innings'][0]['1st innings']['deliveries']#1st innings
+    deliv2 = data['innings'][1]['2nd innings']['deliveries']#2nd innings
+
+    #for getting extras in 1st innings and store in fst list
+    for b in  deliv1:
+        for k in b:
+            if b[k]['runs']['extras'] != 0 :
+                first_innings.append(b[k]['runs']['extras'])
 
 
-    difference =
+    #for getting extras in 1st innings and store in scnd list
+    for b in deliv2:
+        for k in b:
+            if b[k]['runs']['extras'] !=0 :
+                second_innings.append(b[k]['runs']['extras'])
+
+
+
+
+
+    difference = abs(len(first_innings) - len(second_innings))
 
 
     return difference
