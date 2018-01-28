528ef1e1aa7129e59984ccff4a3ced86653312bb
nikhilcss97 <nikhil.css97@gmail.com>
2018-01-28 14:29:51 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -6,6 +6,18 @@ data = read_data()
 def bowled_out(data=data):
 
     # Write your code here
+    bowled_players=[]
+
+    for i in data["innings"][1]["2nd innings"]["deliveries"]:
+
+        for key in i:
+
+             if "wicket" in i[key]:
+
+                 if i[key]["wicket"]["kind"] == "bowled":
+
+                     bowled_players.append(i[key]["batsman"])
+
 
 
     return bowled_players
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -8,7 +8,26 @@ def extras_runs(data=data):
     # Write your code here
 
 
-    difference =
 
+
+    one=0
+    second=0
+
+
+    for i in data["innings"][0]["1st innings"]["deliveries"]:
+
+        for key in i:
+            if i[key]["runs"]["extras"]>0:
+                one+=1
+
+
+    for i in data["innings"][1]["2nd innings"]["deliveries"]:
+
+        for key in i:
+            if i[key]["runs"]["extras"]>0:
+                second+=1
+
+
+    difference = second-one
 
     return difference
