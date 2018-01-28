3a6a809221c8d1bc445f23ba166ae779cdef6610
sejal-varu <sejalvaru@gmail.com>
2018-01-28 14:26:57 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -4,10 +4,8 @@ data = read_data()
 
 # Your Solution
 def first_batsman(data=data):
-
     # Write your code here
-
-
-
+    for ele in data['innings'][0].values():
+	       name =  ele['deliveries'][0][0.1]['batsman']
 
     return name
