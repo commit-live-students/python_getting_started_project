6313875425267b77d32e768aca18fa7c1740ab17
sacvijai <sacvijai@gmail.com>
2018-01-29 06:35:26 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -4,8 +4,8 @@ data = read_data()
 
 # solution
 def teams(data=data):
-
-    # write your code here
-    #teams =
-
+    teams = []
+    for rec in  data.item():
+        if rec[0] ='info' and rec[1]='teams':
+            teams = teams.append [rec]
     return teams
