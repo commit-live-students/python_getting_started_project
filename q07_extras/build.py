d04101bb40eb01265e7c2e35fe24ba5893193413
sejal-varu <sejalvaru@gmail.com>
2018-01-30 17:50:25 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,14 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-
     # Write your code here
+    first_extra = []
+    for k,v in data['innings'][0].iteritems():
+        first_extra = [e['runs']['extras'] for i, x in enumerate(v['deliveries']) for e in x.values() if e['runs']['extras'] > 0]
 
+    secnd_extra = []
+    for k,v in data['innings'][1].iteritems():
+        secnd_extra = [e['runs']['extras'] for i, x in enumerate(v['deliveries']) for e in x.values() if e['runs']['extras'] > 0]
 
-    difference =
-
-
+    difference = len(secnd_extra) - len(first_extra)
     return difference
