66c9b8917a46d2fc46948d2af7b1b363e7ba843a
sacvijai <sacvijai@gmail.com>
2018-01-31 06:32:12 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -4,10 +4,10 @@ data = read_data()
 
 # Your Solution
 def first_batsman(data=data):
-
     # Write your code here
-
-
-
-
+    innings =  data['innings'][0]
+    first_inning = innings['1st innings']
+    deliveries = first_inning['deliveries'][0]
+    first_ball = deliveries[deliveries.keys()[0]]
+    name = first_ball.get('batsman')
     return name
