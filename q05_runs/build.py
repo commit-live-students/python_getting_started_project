04509f3b58f0711b4e65dbd133babbd02d772910
sacvijai <sacvijai@gmail.com>
2018-01-31 08:17:01 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -7,6 +7,11 @@ data = read_data()
 def BC_runs(data):
 
     # Write your code here
-
-
+    innings =  data['innings'][0]
+    deliveries = innings['1st innings']['deliveries']
+    runs = 0
+    for i in deliveries:
+        if i[i.keys()[0]].get('batsman') == 'BB McCullum':
+            ref= i[i.keys()[0]].get('runs')
+            runs += ref.get('batsman')
     return(runs)
