9e9415861aef37f42c94f126d0765ef19f561d06
PBPatil <theprasadpatil@gmail.com>
2018-01-31 09:40:14 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -5,6 +5,15 @@ data = read_data()
 
 # Your Solution
 def BC_runs(data):
+    # Write your code here
+    runs = 0
+    total = data['innings'][0]['1st innings']['deliveries']
+
+    for b in total:
+        for k in b:
+            if b[k]['batsman'] == 'BB McCullum' and b[k]['runs']['batsman'] != 0:
+
+                runs += b[k]['runs']['batsman']
 
     # Write your code here
 
