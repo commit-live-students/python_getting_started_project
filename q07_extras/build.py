cd52d1d911ce0c16579bade0b5c42f59b04bd29e
hiteshsingh91 <hitesh.6131@gmail.com>
2018-02-04 06:36:49 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,37 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    firstinnings_extras = 0
+    for k in data['innings'][0]['1st innings']['deliveries']:
+        for y,x in k.items():
+            ydata =  x['runs']
+            if ydata['extras'] > 0:
+                firstinnings_extras = firstinnings_extras + 1
 
-    # Write your code here
-
-
-    difference =
-
-
+    secondinnings_extras = 0
+    for k in data['innings'][1]['2nd innings']['deliveries']:
+        for y,x in k.items():
+            ydata =  x['runs']
+            if ydata['extras'] > 0:
+                secondinnings_extras = secondinnings_extras + 1
+    difference = secondinnings_extras-firstinnings_extras
     return difference
+#         return difference
+
+
+#
+# firstinnings_extras = 0
+# for k in data['innings'][0]['1st innings']['deliveries']:
+#     for y,x in k.items():
+#         ydata =  x['runs']
+#         if ydata['extras'] > 0:
+#             firstinnings_extras = firstinnings_extras + 1
+# secondinnings_extras = 0
+# second_extras = 0
+# for k in data['innings'][1]['2nd innings']['deliveries']:
+#     for y,x in k.items():
+#         ydata =  x['runs']
+#         if ydata['extras'] > 0:
+#             secondinnings_extras = secondinnings_extras + 1
+# difference = secondinnings_extras-firstinnings_extras
+# print difference
