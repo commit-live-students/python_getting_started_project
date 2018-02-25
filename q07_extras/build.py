bd06dc337ebb0a8741771d8e37e32650fcecddac
kvin15 <vinayakkhamkar@gmail.com>
2018-02-25 19:04:19 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,19 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-
     # Write your code here
+    sum1 = 0
+    for i in range(0,len(data['innings'][0]['1st innings']['deliveries'])):
+        keys1 = data['innings'][0]['1st innings']['deliveries'][i].keys()[0]
+        if int(data['innings'][0]['1st innings']['deliveries'][i][keys1]['runs']['extras']) > 0:
+            sum1 = sum1 + 1
 
+    sum2 = 0
+    for i in range(0,len(data['innings'][1]['2nd innings']['deliveries'])):
+        keys2 = data['innings'][1]['2nd innings']['deliveries'][i].keys()[0]
+        if int(data['innings'][1]['2nd innings']['deliveries'][i][keys2]['runs']['extras']) > 0:
+            sum2 = sum2 + 1
 
-    difference =
-
-
+    difference = sum2 - sum1
+    print difference
     return difference
