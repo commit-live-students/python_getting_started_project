f7183814081a9c371b2ad6c08a70188fcfda0c71
TheSarang <narkhedesarang@gmail.com>
2018-02-25 13:05:07 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -10,12 +10,7 @@ def first_batsman(data=data):
     path = './data/ipl_match.yaml'
     with open(path, mode='r') as file_loader:
         data = yaml.load(file_loader)
-
-    #name = data['innings']['1st innings']['deliveries'][0.1]['batsman']
-    #name = data['innings'][0]['1st innings'][1]['deliveries'][0][0.1][0]['batsman']
-    #name = data['innings'][0]['1st innings'][1]['batsman']
     name = data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
-    #name = 'SC Ganguly'
     return name
 
 
