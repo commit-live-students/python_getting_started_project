0ae2066e9c6dc3b753f39e14c44937a68532f91a
Anand-Radhey <anand.radhey@gmail.com>
2018-03-05 18:36:29 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -4,11 +4,28 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    extras_1 = 0
+    extras_2 = 0
+    extras_list_1 = []
+    extras_list_2 = []
+    difference = 0
+    del_list_1 = data['innings'][0]['1st innings']['deliveries']
+    del_list_2 = data['innings'][1]['2nd innings']['deliveries']
 
-    # Write your code here
+    for i in range(0, len(del_list_1)):
+        for k1, v1 in del_list_1[i].items():
+            #extras_1 += v1['runs']['extras']
+            if (v1['runs']['extras'] > 0):
+                extras_1 += 1
 
+    for i in range(0, len(del_list_2)):
+        for k1, v1 in del_list_2[i].items():
+            #extras_2 += v1['runs']['extras']
+            if (v1['runs']['extras'] > 0):
+                extras_2 += 1
 
-    difference =
-
-
+    difference = extras_2 - extras_1
     return difference
+
+
+print extras_runs(data)
