ee1ac71491734f617fa4111b85200de0bb0a6a8f
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 05:51:06 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,31 @@
+
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 
-# Your Solution
-def BC_runs(data):
+def BC_runs(data=data):
 
-    # Write your code here
-
+    runs = 0
+    deliveries = data['innings'][0]['1st innings']['deliveries']
+    for delivery in deliveries:
+        for delivery_number, delivery_info in delivery.items():
+            if delivery_info['batsman'] == 'BB McCullum':
+                runs += delivery_info['runs']['batsman']
 
     return(runs)
+# Default Imports
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
+data = read_data()
+
+def deliveries_count(data=data):
+
+    count = 0
+    deliveries = data['innings'][0]['1st innings']['deliveries']
+    for delivery in deliveries:
+        for delivery_number, delivery_info in delivery.items():
+            if delivery_info['batsman'] == 'RT Ponting':
+                count += 1
+
+    return count
+
