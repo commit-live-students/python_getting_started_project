0f9a6acfebb008dd9efece226e23b1243bde2325
mihirpanchal4 <mihirpanchal4@yahoo.com>
2018-03-16 08:14:03 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -3,17 +3,10 @@ from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution Here
-ganguly_runs = []
 def deliveries_count(data=data):
-    count = data['innings'][0]['1st innings']['deliveries']
-    for delivery in count:
-        for delivery_number, delivery_info in delivery.items():
-            if delivery_info['batsman'] == 'SC Ganguly':
-                over_and_ball_number = delivery_number
-                runs_scored = delivery_info['runs']['batsman']
-
-                ganguly_runs.append((over_and_ball_number, runs_scored))
-
-
+    count=0
+    for k in data['innings'][0]['1st innings']['deliveries']:
+        for y,x in k.items():
+            if x['batsman']== 'RT Ponting':
+                count=count+1
     return count
-print deliveries_count()
