7c84f7cdd8d706a6f1055a2b46caa60d0d0f8159
mihirpanchal4 <mihirpanchal4@yahoo.com>
2018-03-16 08:32:31 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -4,7 +4,13 @@ data = read_data()
 
 
 # Your Solution
-def BC_runs(data):
+def BC_runs(data = data):
+    runs=0
+    for k in data['innings'][0]['1st innings']['deliveries']:
+        for y,x in k.items():
+            if x['batsman']== 'BB McCullum':
+                runs=runs+x['runs']['batsman']
+    return runs
 
     # Write your code here
 
