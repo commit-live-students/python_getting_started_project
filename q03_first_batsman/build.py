1c38093cf5eec31c482b77f7ec8f42e7f38b3ba2
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-23 06:40:34 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,4 +1,3 @@
-
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,4 +6,5 @@ data = read_data()
 def first_batsman(data=data):
     name = data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
     return name
+
 
