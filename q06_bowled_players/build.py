c84aaaba01484feede75e99b3094c455ff0c4544
asdoc <asdocode@gmail.com>
2018-04-13 17:20:22 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,3 +1,4 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,6 +7,19 @@ data = read_data()
 def bowled_out(data=data):
 
     # Write your code here
+    
+    bowled_players = []
+
+    deliveries = data['innings'][1]['2nd innings']['deliveries']
+
+    for i in deliveries:
+        for key in i:
+            run_var = i[key]
+            if 'wicket' in run_var and run_var['wicket']['kind'] == 'bowled':
+                bowled_players.append(run_var['batsman'])
 
 
     return bowled_players
+
+
+
