4bf614b9bc36a94db81bbd663ed27d869efa76c7
chirag-shinde <shinde.chirag2204@gmail.com>
2018-04-13 07:08:05 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,9 +7,17 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    difference = 0
+    for i in range(len(data['innings'])):
+        for key in data['innings'][i]:
+            for delivery_number in data['innings'][i][key]['deliveries']:
+                for num in delivery_number:
+                    if 'extras' in delivery_number[num]:
+                        if(i % 2 == 0):
+                            difference +=1
+                        else:
+                            difference -= 1
+    return abs(difference)
 
 
-    difference =
 
-
-    return difference
