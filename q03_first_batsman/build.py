6083157a8e0127735396ccd54dffc4260fc8ff29
asdoc <asdocode@gmail.com>
2018-04-13 17:11:43 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -6,10 +6,8 @@ data = read_data()
 # Your Solution
 def first_batsman(data=data):
     # Write your code here
-    innings = data['innings'][0]
-    deliveries = innings['1st innings']['deliveries']
-    name = deliveries[0][0.1]['batsman'] 
-    print(name)
+    return data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman'] 
+
     return name
 
 
