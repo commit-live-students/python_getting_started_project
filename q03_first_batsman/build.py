0b9f2032da41b7c5163cde877c062c77151f8deb
NeehaK <neehakhalfay@gmail.com>
2018-04-14 08:31:29 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,13 +1,22 @@
+# %load q03_first_batsman/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def first_batsman(data=data):
+    name=data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
 
-    # Write your code here
-
-
+    
 
 
     return name
+
+name =data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
+
+name
+type(data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman'])
+abcdef =first_batsman()
+type(abcdef)
+
+
