6da10ac55ff63e22d4203727fb564a1d5b6f7074
siddesh001 <siddesh.001@gmail.com>
2018-04-15 18:58:33 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -5,8 +5,19 @@ data = read_data()
 
 # Your Solution
 def BC_runs(data):
-
+    runs=0
     # Write your code here
 
-
+    main_data=[]
+    slices=(data['innings'][0]['1st innings']['deliveries'])
+    x=0
+    for a in slices:
+        for b in a:
+            main_data.append(slices[x][b])
+            x+=1
+    for a in main_data:
+        if a['batsman']=='BB McCullum':
+            runs+=a['runs']['batsman']
     return(runs)
+
+#rint(BC_runs(data))
