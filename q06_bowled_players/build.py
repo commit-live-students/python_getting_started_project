ee2c7303220766f348ef6f075cf7ab141b85f1da
Nikita1993 <nikitajindal93@gmail.com>
2018-04-15 09:03:17 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,3 +1,4 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,6 +7,16 @@ data = read_data()
 def bowled_out(data=data):
 
     # Write your code here
+    bowled_players=[]
+    deliveries= data['innings'][1]['2nd innings']['deliveries']
+    for d in deliveries:
+        for key,value in d.items():
+            if 'wicket' in value.keys():
+                if value['wicket']['kind']=='bowled':
+                    bowled_players.append(value['wicket']['player_out'])
 
 
     return bowled_players
+
+bowled_out(data)
+
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,9 +7,23 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    d_1= data['innings'][0]['1st innings']['deliveries']
+    d_2= data['innings'][1]['2nd innings']['deliveries']
+    extras1=[]
+    extras2=[]
+    for d in d_1:
+        extras1.extend([d[key]['extras'] for key in d.keys() if 'extras' in d[key].keys()])
+                
+                    
+    for d in d_2:
+        for key,value in d.items():
+            if 'extras' in value.keys():
+                extras2.append(value['extras'])
 
 
-    difference =
-
-
+    difference = len(extras2)-len(extras1)
     return difference
+
+extras_runs(data)
+
+
