ce14a1ed0da1febde75a4cc7f3cacadf30e280f2
swapcy <email2swap@gmail.com>
2018-04-16 16:15:08 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,3 +1,4 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -5,8 +6,15 @@ data = read_data()
 
 # Your Solution
 def BC_runs(data):
-
     # Write your code here
-
+    runs = 0
+    deliveries = data['innings'][0]['1st innings']['deliveries']
+    print(type(deliveries))
+    for d in deliveries:
+        current_deliveries = list(d)
+        delivery_detail = d[current_deliveries[0]]
+        if(delivery_detail['batsman']== 'BB McCullum'):
+            runs = runs + delivery_detail['runs']['batsman']
 
     return(runs)
+
