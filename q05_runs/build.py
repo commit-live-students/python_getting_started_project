775486c648344fec80269db92ab1301de17db6a8
sanchitaharlalka <Sanchitaharlalka@gmail.com>
2018-04-16 01:05:58 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,20 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+import yaml
 
+def read_data():
+    f=open('./data/ipl_match.yaml')
+    datadict=yaml.load(f)
+    return datadict
 
-# Your Solution
-def BC_runs(data):
+def BC_runs(datadict):
+    deliveries=datadict['innings'][0]['1st innings']['deliveries']
+    runs=0
+    for delivery in deliveries:
+        for key,value in delivery.items():
+            batsman=value['batsman']
+            if batsman=='BB McCullum':
+                runs+=value['runs']['batsman']
+    return runs
+datadict=read_data()
+BC_runs(datadict)
+        
 
-    # Write your code here
-
-
-    return(runs)
