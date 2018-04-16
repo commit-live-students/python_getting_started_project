e715741d5679091ca690cfb8d9e354d8e3671e67
sanchitaharlalka <Sanchitaharlalka@gmail.com>
2018-04-16 08:56:40 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,28 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+import yaml
 
-# Your Solution
-def extras_runs(data=data):
+def read_data():
+    f=open('./data/ipl_match.yaml')
+    datadict=yaml.load(f)
+    return datadict
 
-    # Write your code here
+def extras_runs(datadict):
+    deliveries=datadict['innings'][0]['1st innings']['deliveries']
+    extras_1=0
+    for delivery in deliveries:
+        for key,value in delivery.items():
+            
+            if value['runs']['extras'] >0:
+                extras_1+=1     
+            
+    deliveries=datadict['innings'][1]['2nd innings']['deliveries']
+    extras_2=0
+    for delivery in deliveries:
+        for key,value in delivery.items():
+            if value['runs']['extras'] >0:
+                extras_2+=1
+            
+    difference=extras_2-extras_1
+    return(difference)
+datadict=read_data()
+extras_runs(datadict)
 
-
-    difference =
-
-
-    return difference
