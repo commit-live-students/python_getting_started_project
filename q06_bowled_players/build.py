847b0f11959249097502ca84cbf5928ddb43db93
navazsayani <navaz.sayani@somaiya.edu>
2018-04-18 10:58:50 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,3 +1,4 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,6 +7,16 @@ data = read_data()
 def bowled_out(data=data):
 
     # Write your code here
+    ddata = data['innings'][1]['2nd innings']['deliveries']
+    return [x[y]['batsman'] for x in ddata for y in x if 'wicket' in x[y] and x[y]['wicket']['kind'] == 'bowled']
 
 
-    return bowled_players
+[x[y]['batsman'] for x in data['innings'][1]['2nd innings']['deliveries'] for y in x if 'wicket' in x[y] and x[y]['wicket']['kind'] == 'bowled']   
+
+for k in data['innings'][1]['2nd innings']['deliveries']:
+    print (k)
+    for data['innings'][1]['2nd innings']['deliveries'] in k:
+        print 
+    
+
+
