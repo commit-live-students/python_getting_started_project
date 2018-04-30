6c00bc9dc7f41dcb4a0209f602797e646a20558d
sanjarora <sanjarora@gmail.com>
2018-04-30 10:06:57 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,12 +1,15 @@
 import yaml
 
 def read_data():
+ 
+    with open('./data/ipl_match.yaml', 'r') as stream:
+        try:
+            MyDict = yaml.load(stream)
+            print(yaml.load(stream))
+        except yaml.YAMLError as exc:
+            print(exc)
+    return MyDict
 
-    # import the csv file into `data` variable
-    # You can use this path to access the CSV file: '../data/ipl_match.yaml'
-    # Write your code here
 
-    data =
 
-    # return data variable
-    return data
+
