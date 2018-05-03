2c7ecce242b25a08abc00555abf45efee80de2d5
sanjarora <sanjarora@gmail.com>
2018-05-03 21:04:06 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,11 +1,32 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+import yaml
 
-# Your Solution Here
-def deliveries_count(data=data):
-    
-    # Your code here
-    
+def read_data():
+ 
+    with open('./data/ipl_match.yaml', 'r') as stream:
+        try:
+            MyDict = yaml.load(stream)
+            print(yaml.load(stream))
+        except yaml.YAMLError as exc:
+            print(exc)
+    return MyDict
 
+
+TeamDict = read_data()
+nos_of_delivery = len(TeamDict['innings'][0]['1st innings']['deliveries'])
+print(nos_of_delivery)
+
+
+
+def deliveries_count(TeamDict):
+    inningsInfo = TeamDict['innings'][0]
+    deliveries_details = inningsInfo['1st innings']['deliveries']
+    count = 0
+    for dictDetails in deliveries_details:    
+        for x in dictDetails: 
+            for val in dictDetails.values():
+                    if((val['batsman']) == 'RT Ponting'):
+                        count += 1
+   
     return count
+deliveries_count(TeamDict)
+
