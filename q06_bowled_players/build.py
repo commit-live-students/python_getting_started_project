ff9fa3ad7a3afa375e7d7a839be2c147b77b30de
kushalkk90 <kushal_kk90@yahoo.com>
2018-05-04 11:44:16 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,11 +1,25 @@
-# Default Imports
+
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def bowled_out(data=data):
 
-    # Write your code here
-
-
+    bowled_players=list()
+    
+    maintemp = (data['innings'][1]['2nd innings']['deliveries'])
+    len_maintemp = len(maintemp)
+    
+    for ctr in range(len_maintemp):
+        temp_balls=(list(maintemp[ctr]))
+        balls=float(temp_balls[0])
+        
+        if 'wicket' in maintemp[ctr][balls]:
+            if maintemp[ctr][balls]['wicket']['kind'] == 'bowled':
+                bowled_players.append(maintemp[ctr][balls]['wicket']['player_out'])
+    
     return bowled_players
+bowled_out(data)
+bowled_out()
+
+
