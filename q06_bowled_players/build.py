38dec495ee0ba12d873bc082ff4253243f70594d
SL345 <sherilyn345@gmail.com>
2018-05-25 05:10:23 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,11 +1,21 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
+bowled_players = []
 # Your Solution
 def bowled_out(data=data):
 
     # Write your code here
+    lst1 = data['innings'][1]['2nd innings']['deliveries']
+    for value in lst1:
+        for k2,v2 in value.items():
+            try:
+                if v2['wicket']['kind']  == 'bowled':
+                    bowled_players.append(v2['wicket']['player_out'])
+            except KeyError:
+                continue
+
+        return bowled_players
 
 
-    return bowled_players
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,9 +7,18 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
-
-
-    difference =
-
-
+    extra1=extra2=0
+    lst1 = data['innings'][0]['1st innings']['deliveries']
+    for value in lst1:
+        for k2,v2 in value.items():
+            if v2['runs']['extras']!=0:
+                extra1+=1
+    lst2 = data['innings'][1]['2nd innings']['deliveries']
+    for value in lst2:
+        for k2,v2 in value.items():
+            if v2['runs']['extras']!=0:
+                extra2+=1
+    difference =abs(extra1-extra2)
     return difference
+
+
