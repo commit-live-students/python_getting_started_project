7edee87198ed59c01a15b179fe75f28a221f902d
sumitwani <sumitwani281291@gmail.com>
2018-06-02 18:28:00 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,48 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    extra1=[]
+    extra2=[]
+    
+    inf1=data['innings'][0]
+    for k,v in inf1.items():
+        for delivery in v['deliveries']:
+            for a,b in delivery.items():
+                if 'extras' in b.keys():
+                    extra1.append(b['runs']['extras'])
+                    e1=len(extra1)
+                    
+    inf2=data['innings'][1]
+    for k,v in inf2.items():
+        for delivery in v['deliveries']:
+            for a,b in delivery.items():
+                if 'extras' in b.keys():
+                    extra2.append(b['runs']['extras'])
+                    e2=len(extra2)
+    
+    difference=e2-e1
+    
+    
+
+    
+               
+                    
+
+          
+        
+    
 
     # Write your code here
 
 
-    difference =
+    ###difference =
 
 
     return difference
+
+
+
