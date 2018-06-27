227637d14f621248da2bc1be868a26b5d0b687f4
Hegde51 <hegdec29@gmail.com>
2018-06-27 16:26:12 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -4,14 +4,14 @@ from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # solution
+lst = []
+
 def teams(data):
-
+    lst.append(data['info']['teams'])
     # write your code here
-    print(data['info']['teams'])
-
-    return teams
+        
+    return lst[0]
 
 teams(data)
-
-
+lst[0]
 
--- q04_count/build.py
+++ q04_count/build.py
@@ -1,11 +1,24 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution Here
-def deliveries_count(data=data):
-    
-    # Your code here
-    
+lst=[]
 
-    return count
+def deliveries_count(data=data):
+    lst.append(data['innings'][0]['1st innings']['deliveries'])
+    
+     
+    # Your code here
+    count = 0   
+    for balls in lst:
+        if {'batsman': 'RT Ponting'}:
+            count += 1
+        else:
+            continue
+        return balls
+            
+deliveries_count(data)
+data['innings'][0]['1st innings']['deliveries']
+
