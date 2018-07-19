4c44f339a141da8e2ee0fc0cca775b1109696e72
Tusharsharma86 <tusharsharma86@gmail.com>
2018-07-19 09:58:38 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -8,20 +8,26 @@ difference = 0
 # Your Solution
 def extras_runs(data=data):
     
+    #defining variables for extra balls in 2 innings
     extras1 = 0
     extras2 = 0
-    # Write your code here
+    
+    #read the list of balls for 1st innings
     balls_1 = data['innings'][0]['1st innings']['deliveries']
     
+    #iterate through each ball in the list
     for bal in balls_1:
+        #iterate through the ball dictionary for each ball
         for extra in bal.values():
             if extra['runs']['extras'] >0:
                 extras1 += 1
 
-
+    #read the list of balls for 2nd innings
     balls_2 = data['innings'][1]['2nd innings']['deliveries']
     extras2 = 0
+    #iterate through each ball in the list
     for bal in balls_2:
+        #iterate through the ball dictionary for each ball
         for extra in bal.values():
             if extra['runs']['extras'] >0:
                 extras2 += 1
