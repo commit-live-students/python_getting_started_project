0867cffb3ab7ffffd1a09f4d0e746e945f09e2e7
krupa25 <shahkrupa25@gmail.com>
2018-07-20 18:57:46 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,12 +1,16 @@
+# %load q01_read_data/build.py
 import yaml
 
 def read_data():
 
-    # import the csv file into `data` variable
+    # import the csv file into  variable
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
     # Write your code here
 
-    data =
-
+    with open('data/ipl_match.yaml','r') as yamlfile:
+        data = yaml.load(yamlfile)
     # return data variable
     return data
+
+
+
--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,11 +1,20 @@
+# %load q02_teams/build.py
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # solution
 def teams(data=data):
+    teams1 = [x[3] for x in ipl[:1]]
+    teams2 = [x[4] for x in ipl[:1]]
+    team_1=set(teams1)
+    team_2=set(teams2)
+    teams = [team_1.union[team_2]]
 
     # write your code here
     #teams =
 
     return teams
+
+
+
