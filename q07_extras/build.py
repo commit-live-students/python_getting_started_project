82e66576738c8618675e7420cc5f6109d59f76e0
sonalverma1 <sonalverma1@gmail.com>
2018-07-20 08:08:03 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,29 @@
-# Default Imports
+
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
-# Your Solution
-def extras_runs(data=data):
-
-    # Write your code here
-
-
-    difference =
-
-
+delv1 = data['innings'][0]['1st innings']['deliveries']
+delv2 = data['innings'][1]['2nd innings']['deliveries']
+list1 =[]
+list2 =[]
+def extras_runs(data):
+    delv1 = data['innings'][0]['1st innings']['deliveries']
+    delv2 = data['innings'][1]['2nd innings']['deliveries']
+    list1 = 0
+    list2 = 0
+    for i1 in delv1:
+        for a1 in i1.items():
+            if 'extras' in a1[1].keys():
+                list1+=1
+    for i2 in delv2:
+            for a2 in i2.items():
+                if 'extras' in a2[1].keys():
+                    list2+=1
+    difference = abs(list1-list2)
     return difference
+extras_runs(data)
+
+
+
+
+
+
