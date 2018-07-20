8a832d5cead476f7183df82cc7c41ab8c4bc5c4f
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-07-20 18:48:03 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,11 +1,32 @@
+# %load q02_teams/build.py
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
 
+data = read_data()
+teams = teams(data)
+                  
 # solution
 def teams(data=data):
-
+    ipl=[]
     # write your code here
-    #teams =
+    header=tuple(ipl[0])
 
-    return teams
+#set team1
+    team_1 =[x[3] for x in ipl[1:]] 
+    team_1=[]
+    for x in ipl[1:]:
+        team_1.append(x[3])
+    
+    #set team 2
+    team_2=[x[4] for x in ipl[1:]]
+    #change the list s to set for team_1 and team_2
+    team_1=set(team_1) 
+    team_2=set(team_2)
+
+#retrive the unique values from the set
+    teams=list(team_1.union(team_2))
+    #teams =
+    print(teams)
+
+
+
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,20 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-
+    for x,y in zip(inning,extras):
+        if x=='1' and len(y)!=0:
+            ext1.append(int(y))
+        elif x=='2' and len(y)!=0:
+            ext2.append(int(y))
     # Write your code here
-
-
-    difference =
-
+    difference = ext1-ext2
 
     return difference
+
+
+
+
