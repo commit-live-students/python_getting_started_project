0587e4c391eec4e89187feaaaf95cd3d899f9942
GauravSisodia <gaurav4291@gmail.com>
2018-07-21 11:30:41 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,3 +1,4 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,6 +7,17 @@ data = read_data()
 def deliveries_count(data=data):
     
     # Your code here
-    
-
+    count=0
+    overs_balls=data['innings'][0]['1st innings']['deliveries']
+    #print(overs_balls)
+    #print(len(overs_balls))
+    for i in range(len(overs_balls)):
+        for key in overs_balls[i]:
+            if overs_balls[i][key]['batsman']=='RT Ponting':
+                count+=1
     return count
+
+deliveries_count(data)
+
+
+
