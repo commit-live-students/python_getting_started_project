8b7819363cfd82b2e642990a30c55b91cb0d8882
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-07-22 04:42:22 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,32 +1,24 @@
 # %load q02_teams/build.py
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
-
 data = read_data()
-teams = teams(data)
-                  
+print()
 # solution
 def teams(data=data):
-    ipl=[]
-    # write your code here
-    header=tuple(ipl[0])
+#     data['info']=
+#     header=tuple(data[0])
+#     team_Royal_Challengers =[x[3] for x in data[1:]] 
+#     team_Knight_Riders=[x[4] for x in data[1:]]
+#     team_Royal_Challengers=set(team_Royal_Challengers) 
+#     team_Knight_Riders=set(team_Knight_Riders)
+#     teams=team_Royal_Challengers.union(team_Knight_Riders)
+    teams = data['info']['teams']
+    return teams
 
-#set team1
-    team_1 =[x[3] for x in ipl[1:]] 
-    team_1=[]
-    for x in ipl[1:]:
-        team_1.append(x[3])
-    
-    #set team 2
-    team_2=[x[4] for x in ipl[1:]]
-    #change the list s to set for team_1 and team_2
-    team_1=set(team_1) 
-    team_2=set(team_2)
-
-#retrive the unique values from the set
-    teams=list(team_1.union(team_2))
-    #teams =
-    print(teams)
+# retrive teams
+# set 
+# for
+# union
 
 
 
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -5,16 +5,17 @@ data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-    for x,y in zip(inning,extras):
+    for x,y in zip(,extras):
         if x=='1' and len(y)!=0:
             ext1.append(int(y))
         elif x=='2' and len(y)!=0:
             ext2.append(int(y))
-    # Write your code here
-    difference = ext1-ext2
-
-    return difference
-
+    
+        print(sum(ext1))
+        print(sum(ext2))
+        difference=(ext1)-(ext2)
+        
+        
 
 
 
