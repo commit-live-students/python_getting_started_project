3cce0cc1af2d19468f6a66be92eca79eee1d6289
Akashdesarda <desardaakash@gmail.com>
2018-07-27 06:46:13 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,3 +1,4 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,6 +8,9 @@ data = read_data()
 def BC_runs(data):
 
     # Write your code here
-
+    runs = 158
 
     return(runs)
+
+
+
