096bdc3b74f6aafe4cdcb1c231da02f25c80ebc1
rnoshir <noshir92@gmail.com>
2018-07-29 13:44:04 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,11 +1,18 @@
+# %load q01_read_data/build.py
 import yaml
 
 def read_data():
 
-    # import the csv file into `data` variable
+    # import the csv file into  variable
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
-    # Write your code here
-    with open('./data/ipl_match.yaml','r') as my_file:
-        data = yaml.load(my_file)
+    # Write your coprint read_data()de here
+
+    data = yaml.load(open('./data/ipl_match.yaml','r'))
+
     # return data variable
     return data
+
+
+
+
+
