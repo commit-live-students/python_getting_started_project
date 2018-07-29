c983cf47e3f6ff87e6e23c7a282d956ddfd9915b
krupa25 <shahkrupa25@gmail.com>
2018-07-29 06:31:37 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -5,16 +5,11 @@ data = read_data()
 
 # solution
 def teams(data=data):
-    teams1 = [x[3] for x in ipl[:1]]
-    teams2 = [x[4] for x in ipl[:1]]
-    team_1=set(teams1)
-    team_2=set(teams2)
-    teams = [team_1.union[team_2]]
+    
 
     # write your code here
-    #teams =
+    teams = data['info']['teams']
 
     return teams
-
-
+teams()
 
