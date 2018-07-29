2e715a035dba599610dfc1e497bb72a2555a6bbb
soham009 <sohamthaker17@gmail.com>
2018-07-29 06:39:29 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,7 +1,9 @@
 # %load q01_read_data/build.py
 import yaml
 def read_data():
-    data = yaml.load(open('../data/ipl_match.yaml'))
+    with open('data/ipl_match.yaml', 'r' ) as f:
+        data=yaml.load(f)
+        
     return data
 
 
