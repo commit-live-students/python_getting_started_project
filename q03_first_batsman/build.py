e69d86d9e9403725aa215f97a74a1539bae151c7
krupa25 <shahkrupa25@gmail.com>
2018-07-29 06:54:30 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,9 +1,11 @@
+# %load q03_first_batsman/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def first_batsman(data=data):
+    name = data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
 
     # Write your code here
 
@@ -11,3 +13,5 @@ def first_batsman(data=data):
 
 
     return name
+first_batsman()
+
