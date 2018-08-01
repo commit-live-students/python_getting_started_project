1f97583f4672020d2581efe5616259bc324e8c94
sonik8494 <sourabh.niks1994@gmail.com>
2018-08-01 06:46:58 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -2,20 +2,14 @@
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
+print()
 # solution
 def teams(data=data):
-    team_1 = []
-    team_2 = []
-    for x in data[1:]:
-        team_1.append(x[3])
-    for x in data[1:]:
-        team_2.append(x[4])
-    team_1 = set(team_1)
-    team_2 = set(team_2)
-    teams = (team_1.union(team_2))
-
+    teams = data['info']['teams']
+    
+    
     return teams
 
+teams()
 
 
