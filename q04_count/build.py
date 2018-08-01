4061e089dd56f670827ac8d5a0584aba99504064
aishkl <aishkl123@gmail.com>
2018-08-01 02:29:16 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,4 +1,5 @@
-# %load q04_count/build.py
+             
+   # %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,30 +8,21 @@ data = read_data()
 
 # Your Solution Here
 def deliveries_count(data=data):
-    
-    # Your code here
     count=0
-    my='RT Ponting'
     innings=data['innings']
     first=innings[0]
-   
-
     final_inning_list=first['1st innings']['deliveries']
-    #print(type(final_inning_list))
-   
-    #print(final_inning_list)
     for x in final_inning_list:
         for key,value in x.items():
-            final=value
-            #print(final)
-            for k,v in final.items():
-                if my in v:
-                    count += 1
-                              
-          
+            #print('key',key,'value',value)
+            if(value['batsman']=='RT Ponting'):
+                            count=count+1
+    
+    # Your code here
+    
     return count
 
 
 deliveries_count()
-
+           
 
