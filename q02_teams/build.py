73e7596fac5c7b48a0cfff7bfbb0ea46fc78ccaa
sonik8494 <sourabh.niks1994@gmail.com>
2018-08-01 06:37:35 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,11 +1,21 @@
+# %load q02_teams/build.py
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # solution
 def teams(data=data):
-
-    # write your code here
-    #teams =
+    team_1 = []
+    team_2 = []
+    for x in data[1:]:
+        team_1.append(x[3])
+    for x in data[1:]:
+        team_2.append(x[4])
+    team_1 = set(team_1)
+    team_2 = set(team_2)
+    teams = (team_1.union(team_2))
 
     return teams
+
+
+
--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,13 +1,20 @@
+# %load q03_first_batsman/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def first_batsman(data=data):
+    a = data['innings'][0]
+    b = a['1st innings']['deliveries'][0]
+    name = b[0.1]['batsman']
 
-    # Write your code here
+    
 
 
 
 
     return name
+
+
+
