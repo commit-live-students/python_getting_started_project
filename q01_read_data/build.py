10985f7f1cadaa53393b0453d7d1bfa7cf1f82d7
sskewal <sskewal@yahoo.in>
2018-08-02 09:03:25 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,25 +1,17 @@
 # %load q01_read_data/build.py
 import yaml
 import os
-
-os.chdir(r'/home/notebooks/data')
-
-data = {}
-
+os.getcwd()
+os.chdir('/home/notebooks/data')
 def read_data():
 
     # import the csv file into  variable
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
     # Write your code here
-    
-    with open('ipl_match.yaml', 'r') as f:
-        for line in f:
-            data = data + line
-
+    data=yaml.load(open('ipl_match.yaml'))
     return data
-
-
-os.getcwd()
 read_data()
+
+
 
 
--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,32 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
+runs = 0
 
 # Your Solution
 def BC_runs(data):
-
+    runs = 0
+    dely=data['innings'][0]['1st innings']['deliveries']
+    for key in dely:
+        for key,value in key.items():
+            for key1,value1 in value.items():
+                if (key1 == 'batsman')& (value1=='BB McCullum'):
+                    for key2, value2 in value.items():
+                        if key2 == 'runs':
+                            for key3, value3 in value2.items():
+                                if key3 == 'batsman':
+                                    runs = runs + value3
+                                    
+        
     # Write your code here
 
 
     return(runs)
+
+BC_runs(data)
+
+                
+            
+
+
