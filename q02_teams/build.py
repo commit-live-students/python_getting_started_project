7a11bf464563f3cf0b07740dacc0cf36791066f9
sagark93 <sagarkulkarni093@gmail.com>
2018-08-03 14:18:31 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,3 +1,4 @@
+# %load q02_teams/build.py
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -9,3 +10,19 @@ def teams(data=data):
     #teams =
 
     return teams
+
+data = read_data()
+def teams(info):
+    teams = []
+    for x in data['info']['teams']:
+        #if x[0] == info:
+            teams.append(x)
+    return teams
+
+  #  info = {teams:info}
+print(data['info']['teams'])
+teams('info')
+
+data(0)
+
+
