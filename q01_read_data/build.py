6485e00aa90b8f54422b9732fd1cb44ce84f3ff7
Nikhil-Sawant <sawantnikhil31@yahoo.com>
2018-08-03 11:31:33 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,12 +1,14 @@
+# %load q01_read_data/build.py
 import yaml
 
 def read_data():
 
-    # import the csv file into `data` variable
+    # import the csv file into  variable
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
     # Write your code here
-
-    data =
-
-    # return data variable
+    info = open('data/ipl_match.yaml')
+    data = yaml.load(info)
     return data
+
+read_data()
+
