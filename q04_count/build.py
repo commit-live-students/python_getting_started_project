b54dcf9c855905f667e866889eab417935f8635a
KamalShillongwala <qamaldas@gmail.com>
2018-08-05 10:02:57 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,11 +1,22 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
-# Your Solution Here
-def deliveries_count(data=data):
+def deliveries_count(count):
     
     # Your code here
+    count = 0
+    x1 = data['innings']
+    x2 = x1[0]
+    x3 = x2['1st innings']['deliveries']
     
-
+    for index, x in enumerate(x3):
+        x4 = x3[index]
+        for values in x4.values():
+            if values['batsman'] == 'RT Ponting':
+                count = count + 1
     return count
+
+
+
