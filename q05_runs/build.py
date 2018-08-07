890fa4ec08d44df2d74f6f6dce06e2ea5b590a12
raahuul <er.rahul.v27@gmail.com>
2018-08-07 08:50:59 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,3 +1,4 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,6 +8,17 @@ data = read_data()
 def BC_runs(data):
 
     # Write your code here
+    runs=0
+    for dict in data['innings'][0]['1st innings']['deliveries']:
+        
+        for value_dict in dict.values():
+            if value_dict['batsman']=='BB McCullum':
+                runs+=value_dict['runs']['batsman']
 
 
     return(runs)
+
+
+
+
+
