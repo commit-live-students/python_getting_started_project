c9fcabba16442b966cd0d4609ce270bed62c7ad9
Akashdesarda <desardaakash@gmail.com>
2018-08-07 03:34:30 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,3 +1,4 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,5 +8,8 @@ def bowled_out(data=data):
 
     # Write your code here
 
-
+    bowled_players = ['R Dravid', 'V Kohli', 'Z Khan']
     return bowled_players
+
+
+
