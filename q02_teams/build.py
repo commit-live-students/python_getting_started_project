705372121725e47c8af8ec9834a7d219d37a74a7
gomsagar <gomsagar@gmail.com>
2018-08-11 11:05:13 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -1,11 +1,19 @@
+# %load q02_teams/build.py
 # default imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
+import pandas as pd
 data = read_data()
 
 # solution
 def teams(data=data):
-
-    # write your code here
-    #teams =
-
+    info = data['info']
+    teams = info['teams']
     return teams
+
+print(teams(data))
+
+
+
+data
+
+
