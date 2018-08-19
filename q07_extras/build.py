c096c0cc12b66d0ee2215cbe41a7c3deecf37e57
sskewal <sskewal@yahoo.in>
2018-08-19 08:53:23 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -12,22 +12,28 @@ def extras_runs(data):
     for key in one_inning:
         for key1,value1 in key.items():
             for key2,value2 in value1.items():
-                if key2== 'runs':
-                    extra1 = extra1 + value2['extras']
-                
+                if key2== 'extras':
+                    for key3, value3 in value2.items():
+                        extra1 = extra1 + 1
+                        #print(extra1)
+#                 extra1 = extra1 + value2['extras']
+    
     two_inning = data['innings'][1]['2nd innings']['deliveries']
     for key in two_inning:
         for key1,value1 in key.items():
             for key2,value2 in value1.items():
-                if key2== 'runs':
-                    extra2 = extra2 + value2['extras']
+                if key2== 'extras':
+                     for key3, value3 in value2.items():
+                            extra2 = extra2 + 1
+#                             print(extra2)
 
 
     difference = extra1 - extra2
-    difference = difference* -1
-#     print(extra1)
-#     print(extra2)
-#     print(difference)
+    if difference < 0:
+        difference = difference* -1
+    #print(extra1)
+    #print(extra2)
+    #print(difference)
     return difference
 
 extras_runs(data)
