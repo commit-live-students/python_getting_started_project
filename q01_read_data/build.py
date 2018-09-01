787db73e863123028e9f8ae65c36641dc1156ed9
sreelekhaWoonna <sreelekha4877@gmail.com>
2018-09-01 14:31:47 +0000

    Done

--- q01_read_data/build.py
+++ q01_read_data/build.py
@@ -1,12 +1,24 @@
+# %load q01_read_data/build.py
 import yaml
 
 def read_data():
-
-    # import the csv file into `data` variable
+    
+    # import the csv file into  variable
     # You can use this path to access the CSV file: '../data/ipl_match.yaml'
-    # Write your code here
-
-    data =
+    # Write your code here    
+    with open('./data/ipl_match.yaml', 'r') as stream:
+        try:
+            data = yaml.load(stream)
+            
+        except yaml.YAMLError as exc:
+            print(exc)
 
     # return data variable
     return data
+
+import os
+print(os.path.abspath(os.curdir))
+os.curdir.find('../data/ipl_match.yaml')
+
+
+
--- q04_count/build.py
+++ q04_count/build.py
@@ -1,11 +1,33 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution Here
-def deliveries_count(data=data):
-    
+def deliveries_count(data=data):    
     # Your code here
+    for i in range(len(data['innings'][0]['1st innings']['deliveries'])):
+        if  data['innings'][0]['1st innings']['deliveries'][i]['batsman'] == 'RT Ponting':
+            count++
+
     
 
     return count
+
+data = read_data()
+count = 0
+print(data['innings'])
+data = read_data()
+count = 0
+print(data['innings'][0]['1st innings'])
+
+data = read_data()
+count = 0
+
+for i in range(len(data['innings'][0]['1st innings']['deliveries'])):
+        if  data['innings'][0]['1st innings']['deliveries'][i]['batsman'] == 'RT Ponting':
+            count=
+print(count)
+
+
+
