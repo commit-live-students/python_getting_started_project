0404359c4812c436560fc29f66d150e4096dbad4
sreelekhaWoonna <37653727+sreelekhaWoonna@users.noreply.github.com>
2018-09-01 22:32:12 +0530

    Update build.py

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -13,14 +13,3 @@ def bowled_out(data=data):
             if dta[i][key]['wicket']['kind'] == 'bowled':
                 bowled_players.append(dta[i][key]['batsman'])
     return bowled_players
-dta = data['innings'][1]['2nd innings']['deliveries']
-bowled_players =[]
-for i in range(len(dta)):
-    key = list(dta[i].keys())[0]
-    if 'wicket' in dta[i][key]:
-        if dta[i][key]['wicket']['kind'] == 'bowled':
-            print(dta[i][key]['batsman'])
-            bowled_players.append(dta[i][key]['batsman'])
-print(data['innings'][1].keys())
-
-
