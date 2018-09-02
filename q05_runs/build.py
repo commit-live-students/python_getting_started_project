4a68d61d706a7a7375184b0fe48fb152cbeed792
rameshpedagani <rameshpedagani@gmail.com>
2018-09-02 13:04:05 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,20 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
 
 # Your Solution
 def BC_runs(data):
 
     # Write your code here
-
-
+    runs=0;
+    data1=data['innings'][0]['1st innings']['deliveries'];
+    for x in data1:
+        for y in x:
+            if (x[y]['batsman'] == 'BB McCullum'):
+                runs=runs+int(x[y]['runs']['batsman']);
+                
     return(runs)
+
+
+
