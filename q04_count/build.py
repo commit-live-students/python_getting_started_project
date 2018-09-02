1893cce203dc0dd97bd4584fb1bd10be89f718cb
preetiail <ailpreeti@ymail.com>
2018-09-02 15:47:05 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,11 +1,19 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution Here
 def deliveries_count(data=data):
-    
-    # Your code here
-    
-
+    count=0
+    for a in data['innings'][0]['1st innings']['deliveries']:
+        for b in a.values():
+            for c in b.items():
+                if c[0]=='batsman':
+                    if c[1]=='RT Ponting':
+               # if c=='RT Ponting':
+                        count=count+1
+                        print(count)
     return count
+
+
--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,11 +1,13 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def bowled_out(data=data):
-
-    # Write your code here
-
-
+    bowled_players=[]
+    for a in data['innings'][1]['2nd innings']['deliveries']:
+            if data['innings'][1]['2nd innings']['deliveries'][a]['player_out']:
+                bowled_players.extend(data['innings'][1]['2nd innings']['deliveries'][a]['player_out'].values)
     return bowled_players
+
