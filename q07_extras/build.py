b752cd33e1153d97b2d0a685d697b935b4a93881
savans7 <connecttosavan@gmail.com>
2018-09-02 05:13:47 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,20 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+def getExtraBowled(matchDetailsList) :
+    extra=0
+    for index,delInfo in enumerate(matchDetailsList) :
+        for key,value in delInfo.items() :
+            if 'extras' in value:
+                if 'wides' in value['extras']:
+                    extra=extra+1
+                        
+    return extra
+    
+    
+def extras_runs(matchDetails) :
+    one_extras=0
+    sec_extras=0
+    if(len(matchDetails)>0) :
+        one_extras=getExtraBowled(matchDetails['innings'][0]['1st innings']['deliveries'])
+        sec_extras=getExtraBowled(matchDetails['innings'][1]['2nd innings']['deliveries'])
+       
+    return sec_extras-one_extras
 
-# Your Solution
-def extras_runs(data=data):
-
-    # Write your code here
-
-
-    difference =
-
-
-    return difference
