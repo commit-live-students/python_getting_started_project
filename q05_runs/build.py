fe144d49661618f89cdf639d7968eed6337fc27c
savans7 <connecttosavan@gmail.com>
2018-09-02 02:31:57 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,14 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+def BC_runs (matchDetails) :
+    runs=0
+    if(len(matchDetails)>0) :
+        for index,delInfo in enumerate(matchDetails['innings'][0]['1st innings']['deliveries']) :
+            for key,value in delInfo.items() :
+                if value['batsman'] == 'BB McCullum' :
+                    runsScored=value['runs']['batsman']
+                    runs+=runsScored
+    return runs;
 
 
-# Your Solution
-def BC_runs(data):
-
-    # Write your code here
 
 
-    return(runs)
+
