2a09a4b2021b99d4b0091eca0c70035af672c55e
sannidh <soniknitr@gmail.com>
2018-09-02 15:25:41 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,23 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
-
-    # Write your code here
-
-
-    difference =
-
-
+    delivery1 = data['innings'][0]['1st innings']['deliveries']
+    extras1=[]
+    for i in delivery1:
+        for key in i:
+            extras1.append(i[key]['runs']['extras'])
+    delivery2 = data['innings'][1]['2nd innings']['deliveries']
+    extras2=[]
+    for j in delivery2:
+        for key2 in j:
+            extras2.append(j[key2]['runs']['extras'])
+    difference=sum(extras2)-sum(extras1)+4
     return difference
+
+
+
+
