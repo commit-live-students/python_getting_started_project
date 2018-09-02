1e1842bad662e309ff5128ef21050c7b489c3c60
Sandesh373 <dyavanapellisandesh373@gmail.com>
2018-09-02 09:25:19 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,24 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
-
+run=0
 # Your Solution
 def BC_runs(data):
+    rt=data['innings'][0]['1st innings']['deliveries']
+    run=0
+    for x in rt:
+        for k,v in x.items():
+            for p,q in v.items():
+                if p =='batsman' and q =='BB McCullum':
+                    for w,r in v['runs'].items():
+                        if w =='batsman':
+                            run+=r
 
-    # Write your code here
+
+    return(run)
 
 
-    return(runs)
+
+
