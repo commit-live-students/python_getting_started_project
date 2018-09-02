bfb32a3a8f19f77c3e701a8e19999930eab2898b
aksmungekar <mungekarakshay@yahoo.com>
2018-09-02 17:53:46 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,36 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 
 # Your Solution
+runs=0
 def BC_runs(data):
-
-    # Write your code here
-
-
+    runs=0
+    deliveries=data['innings'][0]['1st innings']['deliveries']
+    for balls in deliveries:
+        for k,v in balls.items():
+            for p,q in v.items():
+                if p=='batsman' and q=='BB McCullum':
+                    for w,r in v['runs'].items():
+                        if w=='batsman':
+                            runs=runs+r
     return(runs)
+
+runs=0
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
+data = read_data()
+deliveries=data['innings'][0]['1st innings']['deliveries']
+for balls in deliveries:
+    for k,v in balls.items():
+        for p,q in v.items():
+            if p=='batsman' and q=='BB McCullum':
+                for w,r in v['runs'].items():
+                    if w=='batsman':
+                        runs+=r
+print (runs)
+                        
+        
+
+
