d4ff3a942e11fc77dea0ce1d69bc575dee1ee165
preetiail <ailpreeti@ymail.com>
2018-09-03 15:37:47 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -15,7 +15,7 @@ def extras_runs(data=data):
                         if len(z[1])==1:
                             for me in z[1].values():
                             #print(me)
-                                extra1=extra1+me
+                                extra1=extra1+1
                             #print(extra1) 
                                 
     for p in data['innings'][1]['2nd innings']['deliveries']:
@@ -26,10 +26,12 @@ def extras_runs(data=data):
                         if len(h[1])==1:
                             for me1 in h[1].values():
                             #print(me1)
-                                extra2=extra2+me1
-                            #print(extra2) 
-                                difference =extra1-extra2
-    #print(difference)
+                                extra2=extra2+1
+                            #print(extra2)
+                            #print(extra1)
+                            #print(extra2)
+                                difference =extra2-extra1
+                            #print(difference)
     return difference
 
 
