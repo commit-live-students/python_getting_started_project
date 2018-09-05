3c835830fdd5c1bf140d6cb4951fbdd8fd0f5df9
Jas-simran <simranbright42@gmail.com>
2018-09-05 17:00:33 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,3 +1,4 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -5,7 +6,20 @@ data = read_data()
 # Your Solution Here
 def deliveries_count(data=data):
     
-    # Your code here
     
-
+    count = 0
+    
+    l=data['innings'][0]['1st innings']['deliveries']
+    for x in l:
+        for y in x:
+            if(x[y]['batsman'])=='RT Ponting':
+                count=count+1
+    
+    
     return count
+
+                
+
+
+
+
--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,20 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
+from unittest import TestCase
 
 
 # Your Solution
-def BC_runs(data):
-
-    # Write your code here
 
 
-    return(runs)
+class TestBC_runs(TestCase):
+    def test_BC_runs_return_type()
+        self.assertIsInstance(McCullum_runs, int, 'The Expected type does not match the return type')
+
+    def test_BC_runs_return_values(self):
+        self.assertEqual(158, McCullum_runs, 'The Expected value does not match the return value')
+
+
+
+
