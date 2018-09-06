8cb8ea38ecd721c7c48f29abb82a8d1029ede9fb
Ajpalav <ajit.palav@gmail.com>
2018-09-06 14:41:05 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,29 @@
+# %load q05_runs/build.py
 # Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
-
-
+#om greyatomlib.python_getting_started.q01_read_data.build import read_data
+#ata = read_data()
+#rint(data)
 # Your Solution
-def BC_runs(data):
+#def BC_runs(data):
 
     # Write your code here
 
 
-    return(runs)
+    #return(runs)
+import yaml
+def BC_runs(data):
+    
+    data=open('./data/ipl_match.yaml','r')
+    data1=yaml.load(data)
+    data2=data1['innings'][0]['1st innings']['deliveries']
+    runs=0
+    for key,value in enumerate(data2):
+        for k,v in value.items():
+            if v['batsman']=='BB McCullum':
+                runs+=(v['runs']['batsman'])
+            
+            
+      
+    return runs
+
+
