ca23c900e34725c9c8045da081140380d5e8a0b8
Ajpalav <ajit.palav@gmail.com>
2018-09-06 20:11:53 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,41 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+#%load q07_extras/build.py
 
-# Your Solution
-def extras_runs(data=data):
+import yaml
 
-    # Write your code here
+def extras_runs(data):
+ 
+    data=open('./data/ipl_match.yaml','r')
+    data1=yaml.load(data)
+    data_2nd=data1['innings'][1]['2nd innings']['deliveries']
 
+    data_1st=data1['innings'][0]['1st innings']['deliveries']
+    count1=[]
+    count2=[]
+    for key,value in enumerate(data_2nd):
+       
+        for k,v in value.items():
+            
+            if v['runs']['extras']>0:
+                
+                count2.append(v['runs']['extras'])
+            
+        
+   
 
-    difference =
+    for key1,value1 in enumerate(data_1st):
+       
+        for k1,v1 in value1.items():
+            if v1['runs']['extras']>0:
+                count1.append(v1['runs']['extras'])
 
-
+#print(len(count1))        
+    difference=(len(count2)-len(count1))  
+    
     return difference
+
+
+
+
+
+
+
