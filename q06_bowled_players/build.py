8b99ab11cfa80e035825caed0e5380f1efc0b031
Ajpalav <ajit.palav@gmail.com>
2018-09-06 19:32:27 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,11 +1,30 @@
-# Default Imports
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+#%load q06_bowled_players/build.py
 
-# Your Solution
-def bowled_out(data=data):
+import yaml
 
-    # Write your code here
+def bowled_out(data):
+    data=open('./data/ipl_match.yaml','r')
+    data1=yaml.load(data)
+    data2=data1['innings'][1]['2nd innings']['deliveries']
+    bowled_players=[] 
+    for key,value in enumerate(data2):
+        for k,v in value.items():
+            if 'wicket' in v.keys():
+                if (v['wicket']['kind'])=='bowled':
+                    bowled_players.append(v['wicket']['player_out'])
+#print(bowled_players)
+                #
+            
+            
+    return (bowled_players)        
+            #print(v.keys())
+        #if v.keys() =='wicket':#['wicket']['kind']=='bowled':
+            #print(v)
+        
+            #print(v)
+        
+    #print(type(value))
+#print(data2)
 
 
-    return bowled_players
+
