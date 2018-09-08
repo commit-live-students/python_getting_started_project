272f75360c062d9b461889169b688de685a16109
HarnoorSinghA <harnoorsingh018@gmail.com>
2018-09-08 13:28:49 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,3 +1,4 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -5,8 +6,21 @@ data = read_data()
 
 # Your Solution
 def BC_runs(data):
+    ing=data.get('innings')
+    firsting=ing[0]
+    fingdict=firsting.get('1st innings')
+    deliveries=fingdict.get('deliveries')
+    runs=0
+    for ball in deliveries:
+        bat=list(ball.values())
+        if bat[0].get('batsman')=='BB McCullum':
+            run=bat[0].get('runs').get('batsman')
+            runs=runs+run
 
     # Write your code here
 
 
     return(runs)
+
+BC_runs(data)
+
