83dec6d03482e8e977bd3beaaf4338c0099ce2d2
HarnoorSinghA <harnoorsingh018@gmail.com>
2018-09-08 12:30:59 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,9 +1,19 @@
+# %load q03_first_batsman/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
+#ing=data.get('innings')
+#firsting=ing[0]
+#print(firsting.get('1st innings').get('deliveries')[0].get(0.1).get('batsman'))
+
+
+#print(ing)
 
 # Your Solution
 def first_batsman(data=data):
+    ing=data.get('innings')
+    firsting=ing[0]
+    name=firsting.get('1st innings').get('deliveries')[0].get(0.1).get('batsman')
 
     # Write your code here
 
@@ -11,3 +21,7 @@ def first_batsman(data=data):
 
 
     return name
+
+first_batsman(data)
+
+
