d4dbb46870dcc191ddf8da305223bccda78d3bc8
tracedence <shoaibrashid887@gmail.com>
2018-09-08 13:08:35 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,3 +1,4 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,6 +8,22 @@ data = read_data()
 def BC_runs(data):
 
     # Write your code here
-
+    number_of_bowl = len(data['innings'][0]['1st innings']['deliveries']) #no of ball in 1st innings
+    runs = 0 #to count the number of runs scored by Brendon maCcullum
+    
+    for ball_no in range(number_of_bowl): #loop over each ball
+    
+        #return dictionary of particular ball no
+        di = data['innings'][0]['1st innings']['deliveries'][ball_no]
+        
+        for ke  in di.keys():
+            if data['innings'][0]['1st innings']['deliveries'][ball_no][ke]['batsman'] == 'BB McCullum':
+                
+                runs = runs + data['innings'][0]['1st innings']['deliveries'][ball_no][ke]['runs']['batsman']
+            
 
     return(runs)
+
+BC_runs(data)
+
+
