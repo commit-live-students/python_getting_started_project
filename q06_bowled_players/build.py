6e89292c651f100e1e652d73e23b2035fd3b26c7
dhananjay93 <dhananjay.hawal@gmail.com>
2018-09-09 10:57:11 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,3 +1,4 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,6 +7,18 @@ data = read_data()
 def bowled_out(data=data):
 
     # Write your code here
-
+    bowled_players = []
+    dat = data['innings'][1]['2nd innings']['deliveries']
+    for i in list(range(len(dat))):
+        for key in dat[i]:
+            if 'wicket' in dat[i][key] != None :
+                if dat[i][key]['wicket']['kind'] == 'bowled' :
+                 bowled_players == bowled_players.append(dat[i][key]['wicket']['player_out'])
 
     return bowled_players
+
+bowled_out()
+
+
+
+
--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,33 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def extras_runs(data=data):
+    list1 = []
+    list2 = []
 
     # Write your code here
+    dat1 = data['innings'][0]['1st innings']['deliveries']
+    dat2 = data['innings'][1]['2nd innings']['deliveries']
+    # Write your code here
+    for i in list(range(len(dat1))):
+        for key in dat1[i]:
+            list1.append(dat1[i][key]['runs']['extras'])
+            
 
+    # Write your code here
+    for i in list(range(len(dat2))):
+        for key in dat2[i]:
+            list2.append(dat2[i][key]['runs']['extras'])
 
-    difference =
+    difference = sum(list2) - sum(list1)
 
 
     return difference
+
+
+
+
+
