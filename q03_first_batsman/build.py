d0c3ba42abb6ec0dd71a9f77900e774fb5818de3
ashpandey2509 <ashpandey@yahoo.com>
2018-09-09 11:37:48 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,13 +1,15 @@
-# Default Imports
+# default imports
+from unittest import TestCase
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
+
 data = read_data()
 
-# Your Solution
 def first_batsman(data=data):
+    
+    batsman_name = data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
+    return batsman_name
 
-    # Write your code here
+first_batsman()
 
 
 
-
-    return name
