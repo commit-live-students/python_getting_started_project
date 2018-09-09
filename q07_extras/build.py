8c681bcd7f109b2d4c31a4d077d7a181b17a42dc
tracedence <shoaibrashid887@gmail.com>
2018-09-09 05:27:54 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,9 +7,45 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
-
-
-    difference =
-
+    #for Ist innings
+    
+    list_of_extra_runs_in_1st_innings = []
+    number_of_balls_played = len(data['innings'][0]['1st innings']['deliveries'])
+    
+    
+    for ball_number in range(number_of_balls_played):
+        
+        di = data['innings'][0]['1st innings']['deliveries'][ball_number]
+        
+        for ke in di.keys():
+            
+            if 'extras' in data['innings'][0]['1st innings']['deliveries'][ball_number][ke]:
+                list_of_extra_runs_in_1st_innings.append(data['innings'][0]['1st innings']['deliveries'][ball_number][ke]['extras'])
+     
+       
+    # for 2nd innings
+    
+    list_of_extra_runs_in_2nd_innings = []
+    number_of_balls_played = len(data['innings'][1]['2nd innings']['deliveries'])
+    
+    
+    for ball_number in range(number_of_balls_played):
+        
+        di = data['innings'][1]['2nd innings']['deliveries'][ball_number]
+        
+        for ke in di.keys():
+            
+            if 'extras' in data['innings'][1]['2nd innings']['deliveries'][ball_number][ke]:
+                list_of_extra_runs_in_2nd_innings.append(data['innings'][1]['2nd innings']['deliveries'][ball_number][ke]['extras'])
+                
+                        
+                       
+    
+   
+    difference = len(list_of_extra_runs_in_2nd_innings) - len (list_of_extra_runs_in_1st_innings)
 
     return difference
+
+extras_runs()
+
+
