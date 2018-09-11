4e05964fb65ad852a7195bc5e9ad220a87a5a539
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-11 03:29:50 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,3 +1,4 @@
+# %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,6 +7,24 @@ data = read_data()
 def deliveries_count(data=data):
     
     # Your code here
+    count=0
+    x = data['innings'][0]['1st innings']['deliveries']
     
-
+    for i in x:
+        for k in i.values():
+            print(k['batsman'])
+            if k['batsman'] == 'RT Ponting': 
+                count = count + 1 
+            
     return count
+
+count=0
+x = data['innings'][0]['1st innings']['deliveries']
+for i in x:
+    for k in i.values():
+       print(k['batsman'])
+       if k['batsman'] == 'RT Ponting': 
+            count = count + 1 
+print(count)
+
+
