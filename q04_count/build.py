9766b0e8869cb091bb8ba8bd0158ee5eb40dda63
monildoshi <monild@gmail.com>
2018-09-12 08:21:54 +0000

    Done

--- q04_count/build.py
+++ q04_count/build.py
@@ -1,6 +1,11 @@
 # %load q04_count/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
+
+import pandas as pd
+import numpy as np
+
+
 data = read_data()
 count=0
 ball_faced=[0]
@@ -8,20 +13,18 @@ ball_faced=[0]
 def deliveries_count(data=data):
     count=0
     list_deliveries= data['innings'][0]['1st innings']['deliveries']
+    for delivery in list_deliveries:
+        for delivery_number, delivery_info in delivery.items():
+            if delivery_info['batsman'] == 'RT Ponting':
+                count += 1
+
+    return count
     
     
     
     
                  
     # Your code here
-    
 
-    return count
-
-for i in range (0,len(data['innings'][0]['1st innings']['deliveries'])):
-    data['innings'][0]['1st innings']['deliveries'][i].items()
-    #print(type(data['innings'][0]['1st innings']['deliveries'][i]))        
-temp= data['innings'][0]['1st innings']['deliveries']
-temp
 
 
