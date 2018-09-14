ff5d15f33dd5d326d0d0686c90646cf0dd057308
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-14 05:37:00 +0000

    Done

--- q05_runs/build.py
+++ q05_runs/build.py
@@ -1,12 +1,19 @@
+# %load q05_runs/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
-
 
 # Your Solution
 def BC_runs(data):
 
     # Write your code here
-
-
+    runs = 0
+    varfin=data['innings'][0]['1st innings']['deliveries']
+    for y in varfin: 
+        for v,w in y.items():
+            if (w['batsman']) == 'BB McCullum':
+                runs=runs+(w['runs']['batsman'])
     return(runs)
+
+
+
