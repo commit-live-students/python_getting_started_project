7b029b61f324734cfc38fd7c8084535d3a925cc9
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-14 13:53:02 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -7,8 +8,33 @@ def extras_runs(data=data):
 
     # Write your code here
 
-
-    difference =
-
+    lst1=data['innings'][0]['1st innings']['deliveries']
+    ext1 = []
+    for inlst1 in lst1:
+        for x,y in inlst1.items():
+            for m,n in y.items():
+                if (m == 'runs'):
+                    for r,s in n.items():
+                        if ((r == 'extras') and (s != 0)):
+                            ext1.append(s)
+                            break
+    
+    lst2=data['innings'][1]['2nd innings']['deliveries']
+    ext2 = []                        
+    for inlst2 in lst2:
+        for x,y in inlst2.items():
+            for m,n in y.items():
+                if (m == 'runs'):
+                    for r,s in n.items():
+                        if ((r == 'extras') and (s != 0)):
+                            ext2.append(s)
+                            break
+   
+    difference = len(ext2)-len(ext1)
 
     return difference
+
+extras_runs()
+extras_runs()
+
+
