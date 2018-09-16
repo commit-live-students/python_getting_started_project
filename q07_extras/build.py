539dd02c2fedbe9380718f9705efdc22a56632d4
Sandesh373 <dyavanapellisandesh373@gmail.com>
2018-09-16 08:59:02 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,30 @@
-# Default Imports
+
+
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
+data1=data['innings'][0]['1st innings']['deliveries']
+data2=data['innings'][1]['2nd innings']['deliveries']
 
-# Your Solution
-def extras_runs(data=data):
+def extra_runs(data=data):
+    count = 0
+    for x in data1:
+        for a,b in x.items():
+            for p,q in b.items():
+                if p == 'runs':
+                    for c,d in q.items():
+                        if c == 'extras' and d != 0:
+                            count+=1
+    count1 = 0
 
-    # Write your code here
+    for x in data2:
+        for a,b in x.items():
+            for p,q in b.items():
+                if p == 'runs':
+                    for c,d in q.items():
+                        if c == 'extras' and d != 0:
+                            count1+=1
+    count=count1-count
+    
+    return count
 
 
-    difference =
-
-
-    return difference
