dbc9c2466bc738fa6c816275ddac144910bab4dd
himadri79 <himadrisharma63@gmail.com>
2018-09-20 18:22:23 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,9 +7,27 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
-
-
-    difference =
-
-
+    innings_1 ,innings_2 = 0, 0
+    deliveries1 = data['innings'][0]['1st innings']['deliveries']
+    for delivery in deliveries1:
+        for deli_num,delivery_info in delivery.items():
+            if 'extras' in delivery_info:
+                if 'wides' in delivery_info['extras']:
+                    innings_1 +=delivery_info['extras']['wides']
+                elif 'legbyes' in delivery_info['extras']:
+                    innings_1 += delivery_info['extras']['legbyes']
+    deliveries2 = data['innings'][1]['2nd innings']['deliveries']
+    for delivery in deliveries2:
+        for deli_num,delivery_info in delivery.items():
+            if 'extras' in delivery_info:
+                if 'wides' in delivery_info['extras']:
+                    innings_2 += delivery_info['extras']['wides']
+                elif 'legbyes' in delivery_info['extras']:
+                    innings_2 += delivery_info['extras']['legbyes']
+    print('Total no in innings2: ',innings_2)
+    print('Total no in innings1: ',innings_1)
+    difference = innings_2 - innings_1
     return difference
+extras_runs()
+
+
