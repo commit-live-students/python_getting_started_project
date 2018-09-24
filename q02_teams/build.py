2a021ac059b0de3e12e86bc7f732024817b696f0
geraldhood <gerald.hoody@gmail.com>
2018-09-24 07:29:43 +0000

    Done

--- q02_teams/build.py
+++ q02_teams/build.py
@@ -3,5 +3,8 @@
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
-
+# solution
+def teams(data=data):
+    teams = data['info']['teams']
+    return teams
 
