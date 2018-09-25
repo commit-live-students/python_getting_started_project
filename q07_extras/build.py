2508990abaf0c0c30555a9dd983f298fa96c8cf5
gomsagar <gomsagar@gmail.com>
2018-09-25 16:10:54 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,14 +1,29 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
+
+def getExtras(data, inningNum):
+    extra = 0
+    if(inningNum == 1):
+        deliveries = data['innings'][0]['1st innings']['deliveries']
+    if(inningNum == 2):
+        deliveries = data['innings'][1]['2nd innings']['deliveries']
+    
+    for delivery in deliveries:
+        ballIndex = list(delivery.keys())[0]
+        ball = delivery[ballIndex]
+        if('extras' in ball):
+            extra = extra + 1
+
+    return extra
+        
+    
 def extras_runs(data=data):
-
-    # Write your code here
-
-
-    difference =
-
-
+    difference =  abs(getExtras(data, 1) - getExtras(data, 2))    
     return difference
+extras_runs(data=data)
+
+
