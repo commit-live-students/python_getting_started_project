1ed30722d063dc0652977334df89b77bd999c46f
Jas-simran <simranbright42@gmail.com>
2018-09-26 17:11:34 +0000

    Done

--- q06_bowled_players/build.py
+++ q06_bowled_players/build.py
@@ -1,11 +1,32 @@
+# %load q06_bowled_players/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
 def bowled_out(data=data):
-
-    # Write your code here
-
-
+    
+    #Write your code here
+    l=(data['innings'][1]['2nd innings']['deliveries'])
+    c=[]
+     
+    for x in l:
+        for y in x:
+            if 'wicket' in x[y]:
+                   
+                if 'bowled' in x[y]['wicket']['kind']:
+                    print(x[y]['wicket']['player_out'])
+                        
+                    c.append(x[y]['wicket']['player_out'])
+            
+    bowled_players=c
     return bowled_players
+    
+
+
+        
+                                            
+                                                               
+bowled_out
+
+
