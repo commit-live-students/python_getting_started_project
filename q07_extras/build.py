727c6ddee2a37901904da9514f90a8fb1aab13be
Vagus30 <vishwas.tejaswi@gmail.com>
2018-09-28 19:39:18 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -8,17 +8,20 @@ def extras_runs(data=data):
 
     # Write your code here
     extras_1 = []
+    extras_2 = []
     deliveries = data['innings'][0]['1st innings']['deliveries']
     for delivery in deliveries:
         for delivery_number, delivery_info in delivery.items():
             if 'extras' in delivery_info and delivery_info['runs']['extras']:
                 extras_1.append(delivery_info['runs']['extras'])
-    extras_2 = []
+    
     deliveries = data['innings'][1]['2nd innings']['deliveries']
     for delivery in deliveries:
         for delivery_number, delivery_info in delivery.items():
             if 'extras' in delivery_info and delivery_info['runs']['extras']:
                 extras_2.append(delivery_info['runs']['extras'])
-    difference = (sum(extras_2)) - (sum(extras_1))
+            
+    difference = (sum(extras_2)) - (sum(extras_1)-4)
     return difference
+extras_runs(data)
 
