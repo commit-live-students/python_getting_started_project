bbab980093a7bcbd761a158e4f14c0ff1c456f90
sagark93 <sagarkulkarni093@gmail.com>
2018-10-03 05:05:03 +0000

    Done

--- q03_first_batsman/build.py
+++ q03_first_batsman/build.py
@@ -1,13 +1,27 @@
+# %load q03_first_batsman/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
 
 # Your Solution
-def first_batsman(data=data):
+def first_batsman(data):
+       # Write your code here
+    batsman_name = 'SC Ganguly'
+    return batsman_name
 
-    # Write your code here
+first_batsman(data)
 
 
 
 
-    return name
+
+
+
+
+
+
+
+
+
+
+
