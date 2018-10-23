37d4be72832f29720a0c13073d44f5a7ddd510a9
vaibhav-cric <vaibhavrane.cric@gmail.com>
2018-10-23 23:49:20 +0000

    Done

--- q07_extras/build.py
+++ q07_extras/build.py
@@ -1,3 +1,4 @@
+# %load q07_extras/build.py
 # Default Imports
 from greyatomlib.python_getting_started.q01_read_data.build import read_data
 data = read_data()
@@ -6,9 +7,75 @@ data = read_data()
 def extras_runs(data=data):
 
     # Write your code here
+    
+    l=(data['innings'][0]['1st innings']['deliveries'])
+    E1=[0,0,0]
+    x=0
+    for a in l:
+        for b in a:
+            if 'extras' in a[b].keys():
+                x+=1
+                if 'wides' in a[b]['extras'].keys():
+                    E1[0]+=1
 
+                elif 'legbyes' in a[b]['extras'].keys():
+                    E1[1]+=1
+                else:
+                    E1[2]+=1
+    
+    l2=(data['innings'][1]['2nd innings']['deliveries'])
+    E1=[0,0,0]
+    y=0
+    for a in l2:
+        for b in a:
+            if 'extras' in a[b].keys():
+                y+=1
+                if 'wides' in a[b]['extras'].keys():
+                    E1[0]+=1
 
-    difference =
+                elif 'legbyes' in a[b]['extras'].keys():
+                    E1[1]+=1
+                else:
+                    E1[2]+=1
 
+    difference = y-x
 
     return difference
+
+l2=(data['innings'][1]['2nd innings']['deliveries'])
+E1=[0,0,0]
+x=0
+for a in l2:
+    for b in a:
+        if 'extras' in a[b].keys():
+            x+=1
+            if 'wides' in a[b]['extras'].keys():
+                E1[0]+=1
+                
+            elif 'legbyes' in a[b]['extras'].keys():
+                E1[1]+=1
+            else:
+                E1[2]+=1
+print(E1)
+print(x)
+(data['innings'][1]['2nd innings']['deliveries'][1][0.2]['extras'])
+
+l2=(data['innings'][0]['1st innings']['deliveries'])
+E1=[0,0,0]
+x=0
+for a in l2:
+    for b in a:
+        if 'extras' in a[b].keys():
+                x+=1
+                if 'wides' in a[b]['extras'].keys():
+                    E1[0]+=1
+
+                elif 'legbyes' in a[b]['extras'].keys():
+                    E1[1]+=1
+                else:
+                    E1[2]+=1
+
+h = extras_runs()
+h
+
+
